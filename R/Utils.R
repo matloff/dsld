fa45b3849f0d33b875db969650aa059c331c50f5
Norm Matloff <matloff@cs.ucdavis.edu>
2023-06-17 19:57:23 -0700

    forgot to push

---
+++ R/Utils.R
@@ -0,0 +1,14 @@
+
+# many functions in dsld are wrappers for functions in other packages;
+# in order to avoid "package bloat," we instead check for them as needed
+
+# e.g. say a dsld function f() wraps some function in package p; then
+# instead of listing p as imported etc. in the dsld DESCRIPTION file, 
+# we write the top of f(), getSuggestedLib('p'); this loads p if it is
+# installed on the user's machine, otherwise so informs the user
+
+getSuggestedLib <- function(pkgName)
+   if (!requireNamespace(pkgName,quietly=TRUE))
+      stop(paste0(pkgName, 'not loaded'))
+
+
