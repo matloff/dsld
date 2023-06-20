f5a9f57f13da2b77b5045989299f705259aeef51
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-06-19 17:17:55 -0700

    Added questions (as comments) and sample calls

--- R/CDParcoord/dsldParCoord.R
+++ R/CDParcoord/dsldParCoord.R
@@ -1,9 +1,28 @@
-#' dsldParCoord is a function that plots points in parallel coordinates
-#' @param data data: The data we want to look at
-#' @param yName yName: The variable we want to predict
-#' @param grpName grpName: This determines what lines we are plotting
-#' @return returns a plot with discparcoord
+#' This file stores wrappers for the functions in the cdparcoord pkg
 
+# library(cdparcoord)
+# library(dsld)
+getSuggestedLib('cdparcoord') # Installs cdparcoord on user machine if necessary
+
+
+#' dsldParCoord is a function that plots points in parallel coordinates
+#' @param data The data we want to look at
+#' @param yName The variable we want to predict
+#' @param grpName This determines what lines we are plotting
+#' @return a plot (in parallel coordinates) with discparcoord()
 dsldParCoord <- function(data, yName, grpName) {
+  #' Questions: 
+  #' Do we need to use the prefix cdparcoord::
+  #' How do we decide the k value
+  #' How should we use the graph to predict the Y variable passed in as an arg
   discparcoord(data, k = 500, name = yName, grpcategory = grpName )
 }
+
+# sample data modification as per the quickstart guide
+data(prgeng)
+pe <- prgeng[,c(1,3,5,7:9)]
+pe25 <- pe[pe$wageinc < 250000,]
+pe25 <- makeFactor(pe25,c('educ','occ','sex'))
+pe25disc <- discretize(pe25,nlevels=5)  
+
+dsld(pe25disc, 'pe25 Sex DiscGraph', 'sex') # sample call to plot the graph
