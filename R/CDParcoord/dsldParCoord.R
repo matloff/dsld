e698b54514f24e3ff0011edeeb9543bb67f2459f
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-06-21 15:24:29 -0700

    Added mlb example to code file and rd file

--- R/CDParcoord/dsldParCoord.R
+++ R/CDParcoord/dsldParCoord.R
@@ -1,7 +1,7 @@
 #' This file stores wrappers for the functions in the freqparcoord pkg
 
 library(freqparcoord)
-library(dsld)
+# library(dsld)
 getSuggestedLib('freqparcoord') # Installs cdparcoord on user machine if necessary
 
 #' dsldParCoord is a function that plots points in parallel coordinates
@@ -10,14 +10,18 @@ getSuggestedLib('freqparcoord') # Installs cdparcoord on user machine if necessa
 #' @param grpName What to group the data by
 #' @return a plot (in parallel coordinates) with freqparcoord()
 dsldParCoord <- function(data, m, columns, grpName) {
-  freqparcoord::freqparcoord(data, m, columns, grpvar = grpName )
+  freqparcoord::freqparcoord(data, m, columns, grpvar = grpName)
 }
 
 # sample data modification using the Gender Pay dataset
 # data(prgeng)
 # pe <- prgeng[,c(1,3,5,7:9)]
 # pe25 <- pe[pe$wageinc < 250000,]
+
 # pe25 <- makeFactor(pe25,c('educ','occ','sex'))
-# pe25disc <- discretize(pe25,nlevels=5)  
+# pe25disc <- discretize(pe25,nlevels=5)
 # 
 # dsldParCoord(pe25,10,c(1,5,6),'sex') # sample call to plot the graph
+# 
+# data(mlb)
+# dsldParCoord(mlb,5,4:6,7) # sample call to plot the graph
