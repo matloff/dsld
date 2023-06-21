0b5c815dec1766e0d1c6cc5125c08d8eb8c59a48
adimittal03 <129902929+adimittal03@users.noreply.github.com>
2023-06-21 06:48:31 -0700

    Completed version of dsldTakeALookAround.R

--- R/TakeALook/Dsld_Hwk1.R
+++ R/TakeALook/Dsld_Hwk1.R
@@ -1,24 +1,35 @@
-### Homework 1  - Under Construction
+#' ::: Description :::
+#' @brief This function returns a dataframe containing 4 columns:
+#' 1. Character string consisting of the names of the given feature set. 
+#' 2. Prediction accuracy for yName of this feature set. 
+#' 3. Prediction accuracy for yName of this feature set plus sName. 
+#' 4. Prediction accuracy for sName of this feature set.
+#'
+#' ::: Arguments :::
+#' @param data: dataset
+#' @param yName: as in qeML functions, response variable
+#' @param sName: name of the sensitive variable, an R factor
+#' @param maxFeatureSetSize: maximum # of combinations of features to be returned; default argument set as: maxFeatureSetSize = (ncol(data) - 2)
 
-library(qeML) # Load necessary libraries
-library(Kendall)
-library(docstring)
-
-takeALookAround <- function(data,yName,sName, maxFeatureSetSize = (ncol(data) - 2)) {
-  #' This function returns a dataframe containing 4 columns:
-  #' 1. Character string consisting of the names of the given feature set. 
-  #' 2. Prediction accuracy for yName of this feature set. 
-  #' 3. Prediction accuracy for yName of this feature set plus sName. 
-  #' 4. Prediction accuracy for sName of this feature set. 
-  #' yName and sName are the names of columns in the data; default argument for maxFeatureSetSize = (ncol(data) - 2).
+dsldTakeALookAround <- function(data,yName,sName, maxFeatureSetSize = (ncol(data) - 2)) {
   
-  # Send error message if user enters invalid maxFeatureSetSize
+  # args checking #
   if (maxFeatureSetSize > (ncol(data) - 2)) { 
-    stop("maxFeatureSetSize too large!")
+    stop("maxFeatureSetSize too large!")              # Send error message if user enters invalid maxFeatureSetSize
+  }
+  if (!is.data.frame(data)) {
+    stop("data must be a dataframe.")
   }
   
-  max_features_data = data[,!names(data) %in% c(yName, sName)] # subset dataset to remove sName and yName
-  feature_names = colnames(max_features_data) # get feature set from the column names of our subsetted data
+  # function setup #
+  library(qeML)
+  if (!require('Kendall')) install.packages('Kendall'); library('Kendall')
+  
+  # subset dataset to remove sName and yName
+  max_features_data = data[,!names(data) %in% c(yName, sName)]
+
+  # get names of feature set from our subsetted data
+  feature_names = colnames(max_features_data) 
   
   # initialize empty vectors to populate with data
   col_names = c() 
@@ -26,44 +37,56 @@ takeALookAround <- function(data,yName,sName, maxFeatureSetSize = (ncol(data) - 
   MSE_YS = c()
   MSE_S = c()
   
-  for (i in 1:maxFeatureSetSize) { # run for loop to get all possible combinations of features up to maxFeatureSetSize
-    combination_matrix = combn(feature_names, i) # create combination matrix containing i-features
+  # run for loop to get all possible combinations of features up to maxFeatureSetSize
+  for (i in 1:maxFeatureSetSize) { 
+    combination_matrix = combn(feature_names, i)                    # create combination matrix containing i-features
     
-    for (j in 1:dim(combination_matrix)[2]) { # run for loop to get test accuracies of features across all columns of combination matrix
-      current_features = combination_matrix[,j] # get feature names on the jth loop
-      names = toString(current_features) # convert to string
-      names = gsub(" ","",names) # remove spaces between the characters
-      col_names = c(col_names, names) # append feature names string into vector 
+    # run second for loop across each column of the combination_matrix
+    for (j in 1:dim(combination_matrix)[2]) {       
       
-      feature_data_Y = data[,c(current_features, yName)] # create dataframe with current feature set and Y 
-      feature_data_Y_S = data[,c(current_features, yName,sName)] # create dataframe with current feature set, S and Y
-      feature_data_S = data[,c(current_features, sName)] # create dataframe with current feature set and S
+      # create vector of feature set names across each run - compute 1.
+      current_features = combination_matrix[,j]                     # get feature names on the jth loop
+      names = toString(current_features)                            # convert to string
+      names = gsub(" ","",names)                                    # remove spaces between the characters
+      col_names = c(col_names, names)                               # append feature names string into vector 
       
-      # get part (a) and (b)
-      if (is.numeric(data[[yName]])) { # check whether Y is continuous 
-        a = qeLin(feature_data_Y,yName)$testAcc # get prediction accuracy for Y of this feature set
-        MSE_Y = c(MSE_Y, a) # append test accuracy into vector 
+      # create dataframes to compute test accuracies 
+      feature_data_Y = data[,c(current_features, yName)]            #dataframe with feature set and Y 
+      feature_data_Y_S = data[,c(current_features, yName,sName)]    # dataframe with feature set, S and Y
+      feature_data_S = data[,c(current_features, sName)]            # dataframe with feature set and S
+      
+      # get part 2. and 3.
+      # check whether Y is continuous 
+      if (is.numeric(data[[yName]])) {                  
+        a = qeLin(feature_data_Y,yName)$testAcc                     # get prediction accuracy for Y of this feature set
+        MSE_Y = c(MSE_Y, a)                                         # append test accuracy into vector 
         
-        b = qeLin(feature_data_Y_S,yName)$testAcc # get prediction accuracy for Y of the feature set PLUS s
+        b = qeLin(feature_data_Y_S,yName)$testAcc                   # get prediction accuracy for Y of the feature set PLUS s
         MSE_YS = c(MSE_YS, b)
-      } else { # Y is discrete
-        a = qeLogit(feature_data_Y,yName)$testAcc # get prediction accuracy for Y of this feature set
+      } 
+      # Y is discrete
+      else { 
+        a = qeLogit(feature_data_Y,yName)$testAcc                   # get prediction accuracy for Y of this feature set
         MSE_Y = c(MSE_Y, a)
         
-        b = qeLogit(feature_data_Y_S,yName)$testAcc # get prediction accuracy for Y of this feature set PLUS s
+        b = qeLogit(feature_data_Y_S,yName)$testAcc                 # get prediction accuracy for Y of this feature set PLUS s
         MSE_YS = c(MSE_YS, b)
       }
       
-      # get part(c)
-      if (is.numeric(data[[sName]])) { # check whether sName is continuous 
-        c = qeLin(feature_data_S,sName)$testAcc # get prediction accuracy of S from the feature set
+      # get 4.
+      # check whether sName is continuous
+      if (is.numeric(data[[sName]])) {                        
+        c = qeLin(feature_data_S,sName)$testAcc                     # get prediction accuracy of S from the feature set
         MSE_S = c(MSE_S, c)
-      } else { # if sName is discrete
-        c = qeLogit(feature_data_S,sName)$testAcc # get prediction accuracy of S from the feature set
+      } 
+      # if sName is discrete
+      else { 
+        c = qeLogit(feature_data_S,sName)$testAcc                   # get prediction accuracy of S from the feature set
         MSE_S = c(MSE_S, c)
       }
     }
   }
+  
   # create dataframe
   df <- data.frame(col_names, MSE_Y, MSE_YS, MSE_S) 
   colnames(df)[1] = "Feature Names"
@@ -73,10 +96,5 @@ takeALookAround <- function(data,yName,sName, maxFeatureSetSize = (ncol(data) - 
   return(df)
 }
 
-# run the code
-data(pef)
-takeALookAround(pef, 'wageinc','sex',4) # run w maxFeatureSetSize to get all possible combinations
-takeALookAround(pef, 'wageinc','sex')
-takeALookAround(pef, 'wageinc','sex',1) # run w smaller maxFeatureSetSize
-takeALookAround(pef, 'wageinc','sex',5) # returns error if maxFeatureSetSize too large
-takeALookAround(pef, 'educ','occ',4) # with factor Y
+
+
