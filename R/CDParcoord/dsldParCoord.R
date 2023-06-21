df9e062b9b21f29deeba37b48f9f333966b41f7c
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-06-20 18:08:37 -0700

    Changed the library used from cdparcoord to freqparcoord
    Needs more comments/documentation explaining the results

--- R/CDParcoord/dsldParCoord.R
+++ R/CDParcoord/dsldParCoord.R
@@ -1,28 +1,23 @@
-#' This file stores wrappers for the functions in the cdparcoord pkg
+#' This file stores wrappers for the functions in the freqparcoord pkg
 
-# library(cdparcoord)
-# library(dsld)
-getSuggestedLib('cdparcoord') # Installs cdparcoord on user machine if necessary
-
+library(freqparcoord)
+library(dsld)
+getSuggestedLib('freqparcoord') # Installs cdparcoord on user machine if necessary
 
 #' dsldParCoord is a function that plots points in parallel coordinates
 #' @param data The data we want to look at
-#' @param yName The variable we want to predict
-#' @param grpName This determines what lines we are plotting
-#' @return a plot (in parallel coordinates) with discparcoord()
-dsldParCoord <- function(data, yName, grpName) {
-  #' Questions: 
-  #' Do we need to use the prefix cdparcoord::
-  #' How do we decide the k value
-  #' How should we use the graph to predict the Y variable passed in as an arg
-  discparcoord(data, k = 500, name = yName, grpcategory = grpName )
+#' @param m The number of lines to plot for each group
+#' @param grpName What to group the data by
+#' @return a plot (in parallel coordinates) with freqparcoord()
+dsldParCoord <- function(data, m, columns, grpName) {
+  freqparcoord::freqparcoord(data, m, columns, grpvar = grpName )
 }
 
-# sample data modification as per the quickstart guide
-data(prgeng)
-pe <- prgeng[,c(1,3,5,7:9)]
-pe25 <- pe[pe$wageinc < 250000,]
-pe25 <- makeFactor(pe25,c('educ','occ','sex'))
-pe25disc <- discretize(pe25,nlevels=5)  
-
-dsldParCoord(pe25disc, 'pe25 Sex DiscGraph', 'sex') # sample call to plot the graph
+# sample data modification using the Gender Pay dataset
+# data(prgeng)
+# pe <- prgeng[,c(1,3,5,7:9)]
+# pe25 <- pe[pe$wageinc < 250000,]
+# pe25 <- makeFactor(pe25,c('educ','occ','sex'))
+# pe25disc <- discretize(pe25,nlevels=5)  
+# 
+# dsldParCoord(pe25,10,c(1,5,6),'sex') # sample call to plot the graph
