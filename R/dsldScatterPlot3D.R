0dffce5ee7934bb4e3336e278e075481ab4143e3
Brandon <114265147+zbrandon98@users.noreply.github.com>
2023-06-23 16:12:32 -0700

    Added the ability to add multiple groups with for loop, added ability to make labels and change view-angle

--- R/dsldScatterPlot3D.R
+++ R/dsldScatterPlot3D.R
@@ -1,22 +1,33 @@
-# TODO 
+# TODO
 # documentation
-# gets the group names (catcher, outfielder, etc) automatically
+# gets the group names (catcher, outfielder, etc) automatically (Completed?)
 # create the axes with a for loop
-
-dsldScatterPlot3D <- function (data, axiscols, grpcol, grpVars)
+data("mlb1")
+dsldScatterPlot3D <- function (data, axiscols, grpcol, angle)
 {
-  # function structure
-  # 3 most occurring group members
-  # create an array of dataframes separating each group member
-  # for each of those dataframes, add the 3 other columns to a scatterplot
-  
-  # two separate dataframes
-  # for example the data of only catchers and the data of only outfielders
-  data1 <- data[data[grpcol] == grpVars[1],][axiscols]
-  data2 <- data[data[grpcol] == grpVars[2],][axiscols]
 
-  sp <- scatterplot3d::scatterplot3d(data1[,1], data1[,2], data1[,3], color= "red", angle = 45)
-  sp$points3d(data2[,1], data2[,2], data2[,3], col = "blue")
-  
+  groups <- unique(data[,grpcol]) #Separates column into groups (i.e catcher, outfielder, shortstop, etc)
+
+  #Creates a subset of the data set for each group
+  for(i in 1:length(groups))
+  {
+    group_name <- paste("data", i, sep = "")
+    group <- data[data[grpcol] == levels(groups)[i],][axiscols]
+    assign(group_name, group)
+
+    #Initializes a scatter plot with the first data
+    if (i == 1)
+    { sp <- scatterplot3d(data1[,1], data1[,2], data1[,3], color= "red", angle = angle,
+                          xlab = names(data[axiscols[1]]),
+                          ylab = names(data[axiscols[2]]),
+                          zlab = names(data[axiscols[3]]))
+    }
+
+    #Plots each group as a different color
+    sp$points3d(eval(parse(text=group_name))[,1],
+                eval(parse(text=group_name))[,2],
+                eval(parse(text=group_name))[,3], col = i*20)
+  }
 }
-dsldScatterPlot3D(mlb1, 2:4, 1, c("Catcher", "Outfielder"))
+dsldScatterPlot3D(mlb1, 2:4, 1,10)
+
