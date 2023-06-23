2d193ef8787d0e4c6afc648972368fbc324e1bf4
jotyot <104177975+jotyot@users.noreply.github.com>
2023-06-22 17:26:39 -0700

    infinitely more concise

--- R/dsldScatterPlot3D.R
+++ R/dsldScatterPlot3D.R
@@ -12,26 +12,11 @@ dsldScatterPlot3D <- function (data, axiscols, grpcol, grpVars)
   
   # two separate dataframes
   # for example the data of only catchers and the data of only outfielders
-  data1 <- data[data[grpcol] == grpVars[1],]
-  
-  # we need to convert a list into a float for the scatterplot function
-  # so this converts our dataframe from a list to a float i think
-  # ideally theres a function that does an operation on each row of a matrix but i forgot
-  data1axis <- as.numeric(unlist(data1[axiscols[1]]))
-  for (i in c(axiscols[2:3]))
-  {
-    data1axis <- rbind(data1axis, as.numeric(unlist(data1[i])))
-  }
+  data1 <- data[data[grpcol] == grpVars[1],][axiscols]
+  data2 <- data[data[grpcol] == grpVars[2],][axiscols]
 
-  data2 <- data[data[grpcol] == grpVars[2],]
-  data2axis <- as.numeric(unlist(data1[axiscols[1]]))
-  for (i in c(axiscols[2:3]))
-  {
-    data2axis <- rbind(data2axis, as.numeric(unlist(data2[i])))
-  }
-  
-  sp <- scatterplot3d::scatterplot3d(data1axis[1,], data1axis[2,], data1axis[3,], col= "red", angle = 45)
-  sp$points3d(data2axis[1,], data2axis[2,], data2axis[3,], col = "blue")
+  sp <- scatterplot3d::scatterplot3d(data1[,1], data1[,2], data1[,3], color= "red", angle = 45)
+  sp$points3d(data2[,1], data2[,2], data2[,3], col = "blue")
   
 }
 dsldScatterPlot3D(mlb1, 2:4, 1, c("Catcher", "Outfielder"))
