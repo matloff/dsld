cb3d25af3f5707721b1959b2207a144706e0ed04
Billy Tahirou Ouattara <66051428+BillKG-exe@users.noreply.github.com>
2023-06-23 20:20:56 -0700

    Update takeALook_Py_R.py
    Replaced cvs with our own data frame. Changed yName and sName to match new data frame respectively 'col1' and 'col2'.

--- inst/takeALook_Py_R.py
+++ inst/takeALook_Py_R.py
@@ -1,4 +1,8 @@
+from rpy2.robjects.packages import importr
+
+# Parsing CSV file and storing into a Python dataframe
 import pandas as pd
+from rpy2.robjects import pandas2ri
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
@@ -6,26 +10,34 @@ from rpy2.robjects import pandas2ri
 devtools = importr("devtools")
 
 # This below line may need to be commented
-devtools.install_local("/Users/tahaabdullah/Documents/GitHub/dsld")
+#devtools.install_local("/Users/tahaabdullah/Documents/GitHub/dsld")
 
 dsld = importr("dsld")
 
 # Setting Python args
-yName = 'Electric Range'
-sName = 'Base MSRP'
-maxFeatureSetSize = 2
+yName = 'col1'
+sName = 'col2'
+#maxFeatureSetSize = 1
 
 # Parsing CSV file and storing into a Python dataframe
-data_r = robjects.r("read.csv('Electric_Vehicle_Population_Data.csv')")  # Read CSV file in R
+def pandas_to_r_dataframe(pandas_df):
+    pandas2ri.activate()
+    r_dataframe = pandas2ri.py2rpy(pandas_df)
+    return r_dataframe
+
+pandas_df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [1, 2, 3]})
+# Convert pandas DataFrame to R DataFrame
+data_r = pandas_to_r_dataframe(pandas_df)
+
 
 # Assuming you have the required arguments in Python variables
 
 yName_r = robjects.StrVector([yName])  # Convert variable name to R character vector
 sName_r = robjects.StrVector([sName])  # Convert variable name to R character vector
-maxFeatureSetSize_r = robjects.IntVector([maxFeatureSetSize])  # Convert number to R integer vector
+#maxFeatureSetSize_r = robjects.IntVector([maxFeatureSetSize])  # Convert number to R integer vector
 
 dsldTakeALookAround = dsld.dsldTakeALookAround
-df_r = dsldTakeALookAround(data_r, yName_r, sName_r, maxFeatureSetSize_r)
+df_r = dsldTakeALookAround(data_r, yName_r, sName_r)#, maxFeatureSetSize_r)
 
 #Result stored in a python dataframe
 df_py = pd.DataFrame(df_r)
