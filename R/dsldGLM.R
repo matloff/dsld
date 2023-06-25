d42bff2d1ffb439090500dce577fd678fa646180
adimittal03 <129902929+adimittal03@users.noreply.github.com>
2023-06-25 11:45:50 -0700

    Update dsldGLM.R – Brainstorming

--- R/dsldGLM.R
+++ R/dsldGLM.R
@@ -1,20 +1,51 @@
-# Function to be written, we're currently brainstorming things user can use.
+# Task 2: linear/generalized linear models:
+# Function to be written, we're currently brainstorming things users can utilize.
 
-#' ::: Description :::
-#' @brief This function returns a dataframe containing 4 columns:
-#' 1. Character string consisting of the names of the given feature set. 
-#' 2. Prediction accuracy for yName of this feature set. 
-#' 3. Prediction accuracy for yName of this feature set plus sName. 
-#' 4. Prediction accuracy for sName of this feature set.
-#'
-#' ::: Arguments :::
-#' @param data: dataset given in dataframe format
-#' @param yName: as in qeML functions, response variable
-#' @param sName: name of the sensitive variable, an R factor
-#' @param maxFeatureSetSize: maximum number of combinations of features to be included; default argument set as: maxFeatureSetSize = (ncol(data) - 2)
-
-
-dsldLin <- function(data, yName, sName, xName) {
+dsldModel <- function(data, yName, sName, xName, family) {
   return(NULL)
 }
 
+# DSLD Function
+
+### What to include::
+# 
+
+
+# No interaction --------------------------------------------------------------------
+data(pef)
+z <- glm(formula = wageinc ~ ., family = "gaussian", data = pef)
+summary(z)
+
+
+b <- coef(z)
+C <- vcov(z)
+?vcov()
+u <- c(0,0,0,0,1,0,-1,0,0,0,0)
+
+xx <- t(u) %*% C %*% u
+
+
+
+colnames(vcov(z))
+
+# Full Interaction -------------------------------------------------------------------
+data_split = split(pef,pef[['sex']])
+data_split
+
+
+# male data
+pefm <- data_split$`1`
+drop <- c("sex")
+pefm = pefm[,!(names(pefm) %in% drop)]
+
+# female data
+peff <- data_split$`2`
+drop <- c("sex")
+peff = peff[,!(names(peff) %in% drop)]
+
+
+m <- glm(formula = wageinc ~ ., family = "gaussian", data = pefm)
+f <- glm(formula = wageinc ~ ., family = "gaussian", data = peff)
+
+summary(m)
+summary(f)
