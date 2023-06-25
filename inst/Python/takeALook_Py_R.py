7a3ce1fcc755da6ecc7e1790cb3cbfc3d9a19825
Billy Tahirou Ouattara <66051428+BillKG-exe@users.noreply.github.com>
2023-06-24 17:18:47 -0700

    Update takeALook_Py_R.py
    Assumes that that user input R or pandas data frame and receives pandas data frame as return value from the function.

--- inst/Python/takeALook_Py_R.py
+++ inst/Python/takeALook_Py_R.py
@@ -1,8 +1,10 @@
-from rpy2.robjects.packages import importr
+'''
+    This file contains the interface code for calling the dsldTakeALook from dsld R package.
+    The code uses rpy2 to handle dsld functions call from R and pandas library to check if
+    users data input is in pandas data frame before doing any computation
+'''
 
-# Parsing CSV file and storing into a Python dataframe
 import pandas as pd
-from rpy2.robjects import pandas2ri
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 from rpy2.robjects import pandas2ri
@@ -10,34 +12,60 @@ from rpy2.robjects import pandas2ri
 devtools = importr("devtools")
 
 # This below line may need to be commented
+# Would have to use utils package in order to install dsld from CRAN
+# We used the commented code below to install dsld from our local computer
 #devtools.install_local("/Users/tahaabdullah/Documents/GitHub/dsld")
 
+# dsld package instance. It allows us to call dsld functions inside Python code
 dsld = importr("dsld")
 
-# Setting Python args
-yName = 'col1'
-sName = 'col2'
-#maxFeatureSetSize = 1
 
-# Parsing CSV file and storing into a Python dataframe
-def pandas_to_r_dataframe(pandas_df):
+# This function converts a pandas data frame into an R data frame
+def dsldPandasToRDataframe(pandas_df):
     pandas2ri.activate()
     r_dataframe = pandas2ri.py2rpy(pandas_df)
     return r_dataframe
 
-pandas_df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [1, 2, 3]})
-# Convert pandas DataFrame to R DataFrame
-data_r = pandas_to_r_dataframe(pandas_df)
+
+# This function checks if the data input from the user is in
+# R data frame, pandas' data frame or a different type of data frame.
+# The function converts the data into r's data frame or
+# return -1 which represent an error.
+def dsldIsRDataframe(data):
+    if isinstance(data, robjects.vectors.DataFrame):
+        return data
+    elif isinstance(data, pd.DataFrame):
+        return dsldPandasToRDataframe(data)
+    else:
+        # Error case or csv file or other options
+        return -1
 
 
-# Assuming you have the required arguments in Python variables
+# dsldTakeALookAround function is called inside this function
+# The default value of the maxFeatureSetSize is set to None
+# If No input was received from the user for that argument,
+# we call dsldTakeAlookAround without that parameter. Otherwise,
+# we precise the parameter in the dsldTakeALookAround function.
+# The arguments are passed inside dsldTakeALookAround as r format
+# and the result is returned as Python's pandas data frame.
+def dsldPyTakeALookAround(data, yName, sName, maxFeatureSetSize=None):
+    # Assuming you have the required arguments in Python variables
+    r_data = dsldIsRDataframe(data)
 
-yName_r = robjects.StrVector([yName])  # Convert variable name to R character vector
-sName_r = robjects.StrVector([sName])  # Convert variable name to R character vector
-#maxFeatureSetSize_r = robjects.IntVector([maxFeatureSetSize])  # Convert number to R integer vector
+    yName_r = robjects.StrVector([yName])                               # Convert variable name to R character vector
+    sName_r = robjects.StrVector([sName])                               # Convert variable name to R character vector
 
-dsldTakeALookAround = dsld.dsldTakeALookAround
-df_r = dsldTakeALookAround(data_r, yName_r, sName_r)#, maxFeatureSetSize_r)
+    dsldTakeALookAround = dsld.dsldTakeALookAround
 
-#Result stored in a python dataframe
-df_py = pd.DataFrame(df_r)
+    if maxFeatureSetSize is None:
+        df_r = dsldTakeALookAround(r_data, yName_r, sName_r)
+    else:
+        maxFeatureSetSize_r = robjects.IntVector([maxFeatureSetSize])    # Convert number to R integer vector
+        df_r = dsldTakeALookAround(r_data, yName_r, sName_r, maxFeatureSetSize_r)
+
+    #Result stored in a python pandas' dataframe
+    df_py = pandas2ri.rpy2py_dataframe(df_r)
+
+    return df_py
+
+
