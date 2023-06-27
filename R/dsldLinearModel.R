66d0ab9e1b811b5379159eaa1690a61affd2ed25
Arjun Ashok <arjun3.ashok@gmail.com>
2023-06-26 23:31:39 -0700

    Complete reorganization of Linear Model file. Cleaner syntax, better variable names, more clear comments, more documentation, revitalized class hierarchy, better class structure for easing user experience. Note, polymorphic plot still isn't adjusted to reflect new class hierarchy

--- R/dsldLinearModel.R
+++ R/dsldLinearModel.R
@@ -1,81 +1,115 @@
 # Task 2: linear/generalized linear models:
 # IN PROGRESS - Brainstorming collection of functions that can be used for statistical inferences
 
-### IDEAS:: want to include for single effects of each S levels AND for differences between S levels
 
-# Initial Function: Creates model, outputs S3 list 'dsld' object. This will contain summaries of 
-# the linear models. Allows for interactions vs. no interactions.
+# ------------ Ideas ------------ #
+#' - Want to include for single effects of each S levels AND for differences between S levels
+#' - Initial Function: Creates model, outputs S3 list 'dsld' object. This will contain summaries of 
+#'      the linear models. Allows for interactions vs. no interactions.
+#' - Plot function: Diagnostic plots - could be useful to see how the model diagnostics hold up
+#' - Comparison functions <- not sure how to implement
 
-# Plot function: Diagnostic plots - could be useful to see how the model diagnostics hold up
 
-# comparison functions <- not sure how to implement
+# ------------ Class Design ------------ #
+#' The class hierarchy will be as such:
+#' - dsldDiffModel  :: stores a list of traits such as summary, formula, coefficients, etc. that shows the linear 
+#'                     model produced for each interacting level in the sensitive variable. Stores the following:
+#'      1. formula to go into the model (yName ~.,); [character] @ formula
+#'      2. summary output of model; [character] @ summary
+#'      3. coef of beta parameters; [list] @ coef
+#'      4. data used in the model (useful to see for interactions); [dataframe] @ data
+#'
+#' - dsldLinModel   :: stores a list of dsldDiffModels
 
-library(sandwich) # load libraries 
-library(qeML)
 
-# DSLD Collection of functions to be used ::
-# Linear ---------------------------------------------------------------------------------------
-dsldLinModel <- function(data, yName, sName, interactions = TRUE) {
-  # This functions creates a linear model with/without interactions based on user input of yName, sName, and Data
-  # Will return a list of lists 3 objects:
-  # 1. formula to go into the model (yName ~.,)
-  # 2. summary output of model
-  # 3. coef of beta parameters
-  # 3. data used in the model (useful to see for interactions)
- 
-  dsld = list()  # initialize empty list 
+# ------------ Linear Model ------------ #
+#' ::: Descripton :::
+#' @brief This function produces an instance of the `dsldLinModel` class that 
+#'      houses a separate instance of the `dsldDiffModel` class for each 
+#'      unique, interactive level in the sensitive column specified. The end 
+#'      result is a linear model that with or without interactions that 
+#'      predicts over the inputted data with respect to yName and sName.
+#' 
+#' ::: Arguments :::
+#' @param data: dataset to model over, will be split according to each level 
+#'      in the final outputted `dsldLinModel` object [dataframe]
+#' @param yName: name of the predictive column [character]
+#' @param sName: name of the sensitive column [character]
+#' @param interactions: specifies whether or not to consider interactions. 
+#'      Defaults to TRUE [boolean]
+#' 
+dsldLinModel <- function(data, yName, sName, interactions=TRUE) {
+    # setup linear model #
+    # library requirements
+    library(sandwich)
+    library(qeML)
 
-  # case if interactions == TRUE
-  if (interactions == TRUE) {
-    data_split = split(data,data[[sName]]) # split data by S level 
-    x = names(data_split) # get names 
-    
-    # loop through each S level
-    for (i in x) { 
-      temp_list <- list() # initialize second list, to be populated into main DSLD list 
-      
-      # Get temp_data
-      temp_data <- data_split[[i]] 
-      drop <- c(sName)
-      temp_data = temp_data[,!(names(temp_data) %in% drop)]
-      
-      # get formula & summary output 
-      formula <- as.formula(paste(yName, "~ ."))
-      temp_model <- lm(formula, data = temp_data)
-      
-      temp_list <- c(temp_list, formula, list(summary(temp_model)), list(coef(temp_model)), list(temp_data))
-      names(temp_list) <- c("formula", "summary", "coef", "temp_data")
-      dsld[[i]] <- temp_list
+    # initialize class
+    dsldLinModel = list()
+
+    # interactions #
+    if (interactions == TRUE) {
+        # split data by sensitive level
+        dataSplit = split(data, data[[sName]])
+        dataNames = names(dataSplit)
+        
+        # populate linear model for each level
+        for (name in dataNames) {
+            # initialize instance of dsldDiffModel
+            dsldDiffModel <- list()
+            
+            # data for this level, drop sensitive column
+            diffData <- dataSplit[[i]]
+            drop <- c(sName)
+            diffData <- diffData[, !(names(diffData) %in% drop)]
+            
+            # get formula & diff model
+            formula <- as.formula(paste(yName, "~ ."))
+            diffModel <- lm(formula, data=diffData)
+            
+            # setup instance of dsldDiffModel
+            dsldDiffModel <- c(dsldDiffModel, formula, list(summary(diffModel)), list(coef(diffModel)), list(diffData))
+            names(dsldDiffModel) <- c("formula", "summary", "coef", "data")
+            class(dsldDiffModel) <- "dsldDiffModel"
+
+            # add instance into dsldLinModel
+            dsldLinModel[[name]] <- dsldDiffModel
+        }
+    } 
+    else {
+        # initialize instance of dsldDiffModel
+        dsldDiffModel <- list()
+        
+        # data for non-interactive
+        diffData <- data
+        
+        # get formula & diff model
+        formula <- as.formula(paste(yName, "~ ."))
+        diffModel <- lm(formula, data=diffData)
+        
+        # setup instance of dsldDiffModel
+        dsldDiffModel <- c(temp_list, formula, list(summary(diffModel)), list(coef(diffModel)), list(diffData))
+        names(dsldDiffModel) <- c("formula", "summary", "coef", "data")
+        class(dsldDiffModel) <- "dsldDiffModel"
+
+        # add instance into dsldLinModel
+        dsldLinModel[[sName]] <- dsldDiffModel
     }
-  } 
-  # case if interactions == FALSE 
-  else {
     
-    # get data
-    temp_data <- data 
-    
-    # get formula & summary output 
-    formula <- as.formula(paste(yName, "~ ."))
-    temp_model <- lm(formula, data = temp_data)
-    
-    # populate list
-    temp_list <- c(temp_list, formula, list(summary(temp_model)),list(coef(temp_model)), list(temp_data))
-    names(temp_list) <- c("formula", "summary", "coef", "temp_data")
-    dsld[[1]] <- temp_list
-  }
-  
-  class(dsld) <- 'dsld'
-  return(dsld)
+    # finalize dsldLinModel #
+    class(dsldLinModel) <- "dsldLinModel"
+    return(dsldLinModel)
 }
 
-# Test runs 
+
+# ------------ Linear Model Testing ------------ #
 x <- dsldLinModel(data = pef, yName = 'wageinc', sName = 'sex', interactions = TRUE)
 x # creates a list of two that contains useful info of the linear model. 
 x$`1`
 x$`2`
 
 
-# -------------------------------------------------------------------------------------------------------------
+# ------------ Polymorphic Methods for the Linear Model ------------ #
 #' Defining some basic polymorphic methods for the linear model
 #'  - str()     :: in string form, accesses the summary for the model
 #'  - print()   :: will print the str()
@@ -91,8 +125,8 @@ x$`2`
 #' ::: Arguments :::
 #' @param dsldModel: an instance of the dsldLinearModel s3 object to convert to string representation
 #' 
-str.dsldLinearModel <- function(dsldModel) {
-    # return summary #
+str.dsldLinModel <- function(dsldModel) {
+    # return summaries for now #
     return(summary(dsldModel))
 }
 
@@ -105,7 +139,7 @@ str.dsldLinearModel <- function(dsldModel) {
 #' ::: Arguments :::
 #' @param dsldModel: an instance of the dsldLinearModel s3 object to print
 #' 
-print.dsldLinearModel <- function(dsldModel) {
+print.dsldLinModel <- function(dsldModel) {
     # print string representation #
     print(str(dsldModel))
 }
@@ -118,9 +152,20 @@ print.dsldLinearModel <- function(dsldModel) {
 #' ::: Arguments :::
 #' @param dsldModel: an instance of the dsldLinearModel s3 object to summarize
 #' 
-summary.dsldLinearModel <- function(dsldModel) {
-    # return summary #
-    return(dsldModel$summary)
+summary.dsldLinModel <- function(dsldModel) {
+    # return all summaries #
+    # store summaries
+    summaries <- ""
+
+    # for each interaction level
+    for (level in names(dsldModel)) {
+        # add level and summary
+        summaries <- paste(summaries, "\n :::: ", level, " :::: \n")
+        summaries <- paste(summaries, summary(dsldModel[[level]]$summary))
+    }
+
+    # return concatenated summaries
+    return(summaries)
 }
 
 
@@ -133,7 +178,7 @@ summary.dsldLinearModel <- function(dsldModel) {
 #' ::: Arguments :::
 #' @param dsldModel: an instance of the dsldLinearModel s3 object to print
 #' 
-plot.dsldLinearModel <- function(dsldModel) {
+plot.dsldLinModel <- function(dsldModel) {
     # variable handling #
     data <- dsldModel$data
     linModel <- dsldModel$model
