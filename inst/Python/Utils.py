3806ac49b148581d35467c73f3fd6a717180c590
Billy Tahirou Ouattara <66051428+BillKG-exe@users.noreply.github.com>
2023-06-30 21:50:19 -0700

    Update Utils.py
    Added print function for dsldTakeALookAround_Py_R.py'S shell command usage

--- inst/Python/Utils.py
+++ inst/Python/Utils.py
@@ -20,3 +20,22 @@ def dsld_Rpy2_IsRDataframe(data):
     else: # Error case
         print("Error: not Rdata or Pandas Dataframe")
         return -1
+
+# This function prints the usage for executing dsldTakeALookAround_Py_R.py
+# from the shell command.
+def print_takeALookAround_usage():
+    msg = """
+        Usage: python [dsldTakeALookAround_Py_R.py_path] [data_path] [yName] [sName] [maxFeatureSize]
+
+        Parameters:
+            [dsldTakeALookAround_Py_R.py_path] (string): Path to dsldTakeALookAround_Py_R.py file.
+            [data_path] (string): Path to your data file.
+            [yName] (string): Name of variable to be predicted.
+            [sName] (string): Name of your sensitive variable.
+            [maxFeatureSize] (int): Maximum number of combinations of features to be included in the dataframe; 
+                                    default argument set as: maxFeatureSetSize = (ncol(data) - 2)
+
+        Returns:
+            pandas.DataFrame: The function returns a pandas data frame from the R data frame from the R environment. 
+        """
+    print(msg)
