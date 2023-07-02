40d237d9e64451b0e3e27d7d91b30d2338c83154
adimittal03 <129902929+adimittal03@users.noreply.github.com>
2023-07-02 07:25:58 -0700

    add dsldDffS for no interactions

--- R/dsldLinear.R
+++ R/dsldLinear.R
@@ -1,3 +1,28 @@
+# ------------ Class Design ------------ #
+#' The class hierarchy will be as such:
+#' - dsldDiffModel  :: stores a list of traits such as summary, formula, coefficients, etc. that shows the linear 
+#'                     model produced for each interacting level in the sensitive variable. Stores the following:
+#'      1. formula to go into the model (yName ~.,); [character] @ formula
+#'      2. summary output of model; [character] @ summary
+#'      3. coef of beta parameters; [list] @ coef
+#'      4. data used in the model (useful to see for interactions); [dataframe] @ data
+
+# -------------------------- dsldLinear ------------------------ #
+#' ::: Descripton :::
+#' @brief This function produces an instance of the `dsld` class that 
+#'      houses a separate instance of the `dsldDiffModel` class for each 
+#'      unique, interactive level in the sensitive column specified. The end 
+#'      result is a linear model that with or without interactions that 
+#'      predicts over the inputted data with respect to yName and sName.
+#' 
+#' ::: Arguments :::
+#' @param data: dataset to model over, will be split according to each level 
+#'      in the final outputted `dsldLinModel` object [dataframe]
+#' @param yName: name of the predictive column [character]
+#' @param sName: name of the sensitive column [character]
+#' @param interactions: specifies whether or not to consider interactions. 
+#'      Defaults to TRUE [boolean]
+
 # dsldLinear
 dsldLinear <- function(data, yName, sName, interactions = TRUE) {
   # create list
@@ -105,4 +130,41 @@ dsldGetData <- function(dsld_obj) {
 }
                    
 # dsldGetData(lin_model_1)
+
+### ------------------------- dsldDiffS function -----------------------------------------
+
+dsldDiffS <- function(dsldObj, xName = NULL) {
+  # Load libraries
+  library(multcomp)
+  
+  # no interactions
+  if (length(dsldObj) == 1) {
+    
+    # I'm kinda re-creating the model here, I don't really want to do that - to be updated in dsldLinear later
+    for (i in 1:length(dsldObj)) {
+      model <- glm(dsldObj[[i]]$formula, data = dsldObj[[i]]$data)
+    }
+    
+    # This function gives the necessary info
+    glht_result <- glht(model, linfct = mcp(educ = "Tukey"))
+    summary_glht <- summary(glht_result)
+    
+    # Create a dataframe with the results
+    pairwise_df <- data.frame(
+      Estimate = summary_glht$test$coefficients,
+      Std_Error = summary_glht$test$sigma,
+      P_Value = summary_glht$test$pvalues
+    )
+    
+    return(pairwise_df)
+  } 
+  
+  # yes interactions 
+  else {
+    return('Still making this part :)')
+  }
+}
+
+# run without interactions
+# dsldDiffS(lin_model_2)
                    
