3815bda14743e54f026e889c96ae67fb97d15503
Billy Tahirou Ouattara <66051428+BillKG-exe@users.noreply.github.com>
2023-07-01 19:23:21 -0700

    Update dsldConditDisparity_Py_R.py
    Added more comments

--- inst/Python/dsldConditDisparity_Py_R.py
+++ inst/Python/dsldConditDisparity_Py_R.py
@@ -1,5 +1,7 @@
 '''
-
+    This file contains the interface code for calling the dsldConditDisparity from dsld R package.
+    The code uses rpy2 to handle dsld functions call from R and pandas library to check if
+    users data input is in pandas data frame before doing any computation
 '''
 import pandas as pd
 import sys
@@ -13,10 +15,13 @@ from rpy2.robjects import r
 
 dsld = importr("dsld")
 qeML = importr("qeML")
-ggplot2 = importr('ggplot2')
+ggplot2 = importr('ggplot2')  # May remove this line
 grdevices = importr('grDevices')
 
 
+# This is the interface function for R's dsldConditDisparity function
+# The arguments are converted into R data type before calling dsldConditDisparity function
+# This function uses qeML's qeKNN function as default argument for qeFtn
 def dsldPyConditDisparity(data, yName, sName, xName, condits, qeFtn=qeML.qeKNN, minS=50, yLim=None, useLoess=True):
     r_data = dsld_Rpy2_IsRDataframe(data)
 
@@ -24,7 +29,7 @@ def dsldPyConditDisparity(data, yName, sName, xName, condits, qeFtn=qeML.qeKNN, 
     sName_r = robjects.StrVector([sName])  # Convert variable name to R character vector
     xName_r = robjects.StrVector([xName])  # Convert variable name to R character vector
     condits_r = robjects.StrVector([cond for cond in condits])  # Convert variable name to R character vector
-    minS_r = robjects.IntVector([minS])
+    minS_r = robjects.IntVector([minS])    # Convert variable name to R;s number type
 
     yLim_r = robjects.NULL
     print(sName)
@@ -36,17 +41,11 @@ def dsldPyConditDisparity(data, yName, sName, xName, condits, qeFtn=qeML.qeKNN, 
     useLoess_r = robjects.BoolVector([useLoess])
 
     dsld.dsldConditDisparity(r_data, yName_r, sName_r, xName_r, condits_r, qeFtn, minS_r, yLim_r, useLoess_r)
-    # time.sleep(True)
 
-    # # Save the plot as an image file in R
-    # robjects.r('ggsave("fig.png")')
-    # #robjects.r('png(file="plot.png")')
-    # # Load the image file in Python
-    # image = Image.open("fig.png")
-    # image.show()  # Display the plot using the default image viewer
-
+    # Copy and saves the image as plot.png
     grdevices.dev_copy(device=r.png, filename="plot.png")
     grdevices.dev_off()
+    
     # Load the image file in Python
     image = Image.open("plot.png")
     image.show()  # Display the plot using the default image viewer
@@ -54,26 +53,30 @@ def dsldPyConditDisparity(data, yName, sName, xName, condits, qeFtn=qeML.qeKNN, 
     return
 
 
-# robjects.r['data']('pef')
-# pef = robjects.r['pef']
+# The code below is for testing purposes. We'll remove it 
+# once we're done working on the shell command inputs
+# The code uses pef data for testing
+robjects.r['data']('pef')
+pef = robjects.r['pef']
 
-# dsldPyConditDisparity(pef, "age", "sex", "wageinc", ['age<=60', 'wkswrkd>=25'])
+dsldPyConditDisparity(pef, "age", "sex", "wageinc", ['age<=60', 'wkswrkd>=25'])
 
-# grdevices.dev_copy(device=r.png, filename="plot.png", bg=)
-# grdevices.dev_off()
 
 '''
     For testing: 
 '''
 
 
+# We'll do some additional work on the shell command
+# The code below is not currently working properly
+'''
 if __name__ == "__main__":
     args = sys.argv
     file_path = args[1]
 
     data = pd.read_csv(file_path)
 
-    dsldPyConditDisparity(data, args[2], args[3], args[4], sys.argv[5].split(','))
+    dsldPyConditDisparity(data, args[2], args[3], args[4], sys.argv[5].split(','))'''
 
 
 # python dsldConditDisparity_Py_R.py ../../data/pefFixed.csv age gender wageinc age=60,wkswrkd=25
