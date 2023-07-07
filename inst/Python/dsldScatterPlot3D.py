bcf0787fc6edb8f20c4de52dd2a9744dd4165fe2
Brandon <114265147+zbrandon98@users.noreply.github.com>
2023-07-07 16:28:43 -0700

    minor fixes to helper functions

--- inst/Python/dsldScatterPlot3D.py
+++ inst/Python/dsldScatterPlot3D.py
@@ -1,7 +1,8 @@
-#OVERVIEW:  No need to access dsldScatterPlot3D from dsld-package.
-#           The function uses the package plotly in R, which is 
-#           also available in Python. This file requires packages:
-#           pandas, plotly, pyreadr
+#OVERVIEW:  
+#   No need to access dsldScatterPlot3D from dsld-package.
+#   The function uses the package plotly in R, which is also available in Python.
+#   This file requires packages: pandas, plotly, pyreadr
+#           
 
 #TODO:
 #   Remove hardcoded example (pef) in dsldScatterPlot to accept any dataset  
@@ -11,8 +12,8 @@
 #       I believe this part should be done near the end when dsldScatterPlot is fully/nearly functioning
 #   Change name to dsldPlotly?
 
-import pyreadr #convert .Rdata to panda's dataframe
-import pandas as pd #Convert csv to panda's dataframe
+import pyreadr #convert .rData to panda's data frame
+import pandas as pd #Convert .csv to panda's data frame
 import plotly.express as px #Create interactive graphs
 
 def dsldScatterPlot(data):
@@ -20,7 +21,7 @@ def dsldScatterPlot(data):
     fig = px.scatter_3d(df, x='age', y='wageinc', z='wkswrkd') #z,y,x values must be column names of dataframe
     fig.show()
 def getFileExt(data):
-    return data.split(".",1)[1]
+    return data.split(".",1)[1].lower()
 def csvToPdf(data):
     #Converts any .csv file to a panda's dataframe
     pdf = pd.read_csv(data)
@@ -33,8 +34,8 @@ def rDataToPdf(data):
     return pdf
 def universalConverterToPdf(data):
     fileExt = getFileExt(data)
-    if fileExt == ".csv": return csvToPdf(data)
-    if fileExt == "rData": return rDataToPdf(data)
+    if fileExt == "csv": return csvToPdf(data)
+    if fileExt == "rda" or fileExt == "rdata": return rDataToPdf(data)
     else: 
         print("File extension must be .csv or .rData")
         exit(1)
@@ -46,7 +47,7 @@ ________________________________________________________________________________
 #           Run the next few lines (by uncommenting this example), and it should open interactive graph in browser. 
 #           In terminal, I did: python3 dsldScatterPlot3D.py
   
-df1 = px.data.iris() #data type of df: pandas.core.frame.DataFrame (i.e: pandas dataframe)
+df1 = px.data.iris() #data type of df: pandas.core.frame.DataFrame (i.e: pandas data frame)
 fig = px.scatter_3d(df1, x='sepal_length', y='sepal_width', z='petal_width',
               color='species')
 fig.show()
@@ -54,7 +55,7 @@ fig.show()
 ___________________________________________________________________________________________________________________________
 #EXAMPLE 2: Use either example to test dsldScatterPlot
 
-dsldScatterPlot("/Users/bdzarate98/Documents/GitHub/dsld/data/pefFixed.csv")
+dsldScatterPlot("C:/Users/Brandon/Documents/dsld/data/pefFixed.csv")
 dsldScatterPlot("C:/Users/Brandon/Documents/dsld/data/pef.rData")
 
 ___________________________________________________________________________________________________________________________
