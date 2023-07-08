ebbfc03802a9e006b37952cefda26d8a596473fc
Shubhada Martha <103222835+smartha2003@users.noreply.github.com>
2023-07-07 22:49:37 -0700

    dsldScatterPlot takes in arguments

--- inst/Python/dsldScatterPlot3D.py
+++ inst/Python/dsldScatterPlot3D.py
@@ -5,8 +5,10 @@
 #           
 
 #TODO:
-#   Remove hardcoded example (pef) in dsldScatterPlot to accept any dataset  
-#   Incorporate more arguments from dsldScatterPlot3D R function to Python's equivalent function
+#   Remove hardcoded example (pef) in dsldScatterPlot to accept any dataset  || I think finished this - Shubhada
+#   Incorporate more arguments from dsldScatterPlot3D R function to Python's equivalent function || Work in progress - Shubhada
+#   Edit the getFileExt() so that it can take in the example path: "../../data/pefFixed.csv"
+#       * Basically get this working: dsldScatterPlot("../../data/pefFixed.csv", "age", "wageinc", "wkswrkd")
 #   Possibly add more datatypes accepted through universalConverterToPdf function
 #   Make functions accessible from console
 #       I believe this part should be done near the end when dsldScatterPlot is fully/nearly functioning
@@ -16,22 +18,36 @@ import pyreadr #convert .rData to panda's data frame
 import pandas as pd #Convert .csv to panda's data frame
 import plotly.express as px #Create interactive graphs
 
-def dsldScatterPlot(data):
+# dsldScatterPlot3D <- function(data, sName=NULL, yNames=NULL, sGroups=NULL, 
+                            #   sortedBy="Name", numGroups=8, maxPoints=NULL,
+                            #   xlim=NULL, ylim=NULL, zlim=NULL,
+                            #   main=NULL, colors="Paired", opacity=1, 
+                            #   pointSize=8)
+
+def dsldScatterPlot(data, sName, yName, sGroups):
+
     df = universalConverterToPdf(data)
-    fig = px.scatter_3d(df, x='age', y='wageinc', z='wkswrkd') #z,y,x values must be column names of dataframe
+
+    #fig = px.scatter_3d(df, x='age', y='wageinc', z='wkswrkd') #z,y,x values must be column names of dataframe
+    fig = px.scatter_3d(df, sName, yName, sGroups) #z,y,x values must be column names of dataframe
+
     fig.show()
-def getFileExt(data):
+
+def getFileExt(data): # only works with the path to dataset on the users device within dsld
     return data.split(".",1)[1].lower()
+
 def csvToPdf(data):
     #Converts any .csv file to a panda's dataframe
     pdf = pd.read_csv(data)
     return pdf
+
 def rDataToPdf(data):
     #Converts any .rData file to a panda's dataframe
     dictResult = pyreadr.read_r(data)
     pdfName = list(dictResult.keys())[0]
     pdf = dictResult[pdfName]
     return pdf
+
 def universalConverterToPdf(data):
     fileExt = getFileExt(data)
     if fileExt == "csv": return csvToPdf(data)
@@ -53,7 +69,15 @@ fig = px.scatter_3d(df1, x='sepal_length', y='sepal_width', z='petal_width',
 fig.show()
 
 ___________________________________________________________________________________________________________________________
-#EXAMPLE 2: Use either example to test dsldScatterPlot
+NEWEXAMPLE 2: Copy paste this outside of the comment box with the right file path:
+.csv:
+dsldScatterPlot("/Users/shubhadamartha/Documents/GitHub/dsld/data/pefFixed.csv", "age", "wageinc", "wkswrkd")
+
+.rData:
+dsldScatterPlot("/Users/shubhadamartha/Documents/GitHub/dsld/data/pef.rData", "age", "wageinc", "wkswrkd")
+
+
+OLDEXAMPLE 2: Use either example to test dsldScatterPlot
 
 dsldScatterPlot("C:/Users/Brandon/Documents/dsld/data/pefFixed.csv")
 dsldScatterPlot("C:/Users/Brandon/Documents/dsld/data/pef.rData")
