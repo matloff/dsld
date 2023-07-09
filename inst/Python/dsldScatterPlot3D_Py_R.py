c28a6e0bc69a742b9818ead6cfbefd467d8856d2
Shubhada Martha <103222835+smartha2003@users.noreply.github.com>
2023-07-08 19:17:10 -0700

    Arguments sName and yName fully implemented

--- inst/Python/dsldScatterPlot3D_Py_R.py
+++ inst/Python/dsldScatterPlot3D_Py_R.py
@@ -1,21 +1,25 @@
-import sys
-import os
-import pandas as pd
-from PIL import Image
+# import sys
+# import os
+# import pandas as pd
+# from PIL import Image
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 from Utils import dsld_Rpy2_IsRDataframe
-from rpy2.robjects import r
+# from rpy2.robjects import r
 
 dsld = importr("dsld")
 qeML = importr("qeML")
-ggplot2 = importr('ggplot2')  # May remove this line
-grdevices = importr('grDevices')
+# ggplot2 = importr('ggplot2')  # May remove this line
+# grdevices = importr('grDevices')
 
 rplotly = importr("plotly")
 from IPython.display import display
+R_NULL = robjects.NULL
 
-def dsldPyScatterPlot3D(data, sName = None, yName = None):
+#TODO:
+#Implement the yName to take in both strings and numbers in the vector input
+
+def dsldPyScatterPlot3D(data, sName = R_NULL, yName = R_NULL):
     r_data = dsld_Rpy2_IsRDataframe(data)
 
     if type(sName) == str:
@@ -23,18 +27,24 @@ def dsldPyScatterPlot3D(data, sName = None, yName = None):
     elif type(sName) == int:
         sName_r = robjects.IntVector([sName])    # Convert number to R integer vector
     else:
-        if sName != None:
+        if sName != R_NULL:
             print("ERROR: sName needs to be a string or integer. You Entered: ", sName)
             exit(1)
-    
-    # Figure out if we can do this without so many if statements
-    if sName == None & yName == None:
-        scatter_plot = dsld.dsldScatterPlot3D(r_data)
+        else:
+            sName_r = sName
+
+    if yName == R_NULL:
+        yName_r = yName
+    elif all(y.isdigit() for y in yName):
+        yName_r = robjects.IntVector([int(x) for x in yName])  # Convert 'yName' to an R integer vector
     else:
-        scatter_plot = dsld.dsldScatterPlot3D(r_data, sName_r)
+        yName_r = robjects.StrVector(yName)
+
+    scatter_plot = dsld.dsldScatterPlot3D(r_data, sName_r, yName_r)
 
     # Convert the plot to a Plotly widget
-    plot_widget = rplotly.toWidget(scatter_plot)
+    #plot_widget = rplotly.toWidget(scatter_plot)
+    plot_widget = rplotly.as_widget(scatter_plot)
 
     # Display the interactive Plotly graph in Python
     display(plot_widget)
@@ -43,10 +53,14 @@ def dsldPyScatterPlot3D(data, sName = None, yName = None):
 Need to install.packages("plotly") in R
 pip install IPython
     python
-    from dsldRpy2Scatterplot import dsldPyScatterPlot3D
+    from dsldScatterPlot3D_Py_R import dsldPyScatterPlot3D
     import rpy2.robjects as robjects
     robjects.r['data']('pef')
     data = robjects.r('pef')
     dsldPyScatterPlot3D(data, "sex")
     dsldPyScatterPlot3D(data, 4)
+
+Other Examples:
+    from dsldScatterPlot3D_Py_R import dsldPyScatterPlot3D;import rpy2.robjects as robjects;robjects.r['data']('pef');data = robjects.r('pef');dsldPyScatterPlot3D(data, "sex", ['occ', 'wageinc', 'wkswrkd'])
+    from dsldScatterPlot3D_Py_R import dsldPyScatterPlot3D;import rpy2.robjects as robjects;robjects.r['data']('pef');data = robjects.r('pef');dsldPyScatterPlot3D(data, "sex", ['3', '5', '6'])
 '''
