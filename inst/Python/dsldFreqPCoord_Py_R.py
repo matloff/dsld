f667c996651b59e17d5220601cbff207e3bab11d
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-07-14 21:44:42 -0700

    Most of the args working properly
    TODO: sName only takes in a string name, not a column number. Docs need updating

--- inst/Python/dsldFreqPCoord_Py_R.py
+++ inst/Python/dsldFreqPCoord_Py_R.py
@@ -36,21 +36,20 @@ import sys
 # dsld package instance. It allows us to call dsld functions inside Python code
 dsld = importr("dsld")
 
+# For handling null arguments
+R_NULL = robjects.NULL
+
 # dsldFreqPCoord function is called inside this function
 # The arguments are passed inside dsldFreqPCoord as r format
 # and the result is a graph handled by R.
-
-# method, faceting, k, klm, keepidxs, plotidxs, randclrs, cls
-def dsldPyFreqPCoord(data, m, columns = None, sName, method = "maxdens", faceting = "vert", k = 50, klm = 5*k, keepidxs, plotidxs = False, randclrs = False, clsP):
-    R_NULL = robjects.NULL
-
+def dsldPyFreqPCoord(data, m, columns = R_NULL, sName = R_NULL, method = "maxdens", faceting = "vert", k = 50, klm = R_NULL, keepidxs = R_NULL, plotidxs = False, randclrs = False, clsP = R_NULL):
     # Assuming you have the required arguments in Python variables
     r_data = dsld_Rpy2_IsRDataframe(data) # At this point, data is always intended to be in R dataframe format
     
     m_r = robjects.IntVector([m])  
 
     # Columns can be entered in shell as all strings or all ints
-    if columns = None:
+    if columns == R_NULL:
         robjects.r.assign("r_data", r_data)  
         robjects.r('columns_r <- 1:ncols(r_data)')
         columns_r = robjects.r("columns_r")
@@ -68,9 +67,12 @@ def dsldPyFreqPCoord(data, m, columns = None, sName, method = "maxdens", facetin
     method_r = robjects.StrVector([method])
     faceting_r = robjects.StrVector([faceting])
     k_r = robjects.IntVector([k])
+
+    if klm == R_NULL:
+        klm = 5*k # Couldn't be done in function definition due to k not being recognized
     klm_r = robjects.IntVector([klm])
 
-    if keepidxs == R_NULL
+    if keepidxs == R_NULL:
         keepidxs_r = keepidxs
     else:
         keepidxs_r = robjects.IntVector([keepidxs])
@@ -78,10 +80,10 @@ def dsldPyFreqPCoord(data, m, columns = None, sName, method = "maxdens", facetin
     plotidxs_r = robjects.BoolVector([plotidxs])
     randclrs_r = robjects.BoolVector([randclrs])
 
-    if clsP == R_NULL
+    if clsP == R_NULL:
         cls_r = clsP
     else:
-        cls_r = robjects.StrVector(clsP) # TODO
+        cls_r = robjects.StrVector(clsP) # TODO: what type is this?
 
     # All necessary arguments are in R format at this point
 
@@ -89,7 +91,7 @@ def dsldPyFreqPCoord(data, m, columns = None, sName, method = "maxdens", facetin
     plot_filename = "freqp_coord.png"
 
     # Calling the R function
-    dsld.dsldFreqPCoord(r_data, m_r, columns_r, sName_r, plot_filename)
+    dsld.dsldFreqPCoord(r_data, m_r, columns_r, sName_r, method_r, faceting_r, k_r, klm_r, keepidxs_r, plotidxs_r, cls_r, plot_filename)
 
     # Load and display the saved image in Python
     image = Image.open(plot_filename)
