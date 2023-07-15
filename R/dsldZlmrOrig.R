2798a89f72f43e609e37dd83242c92b7d21289d2
Brandon <114265147+zbrandon98@users.noreply.github.com>
2023-07-14 17:45:24 -0700

    dsldZlmrOrig wrapper for fairml::zlmr.orig

---
+++ R/dsldZlmrOrig.R
@@ -0,0 +1,11 @@
+#dsldZlrmOrig(), a wrapper for fairml:Zlrm.orig()
+#usage:
+#zlrm.orig(response, predictors, sensitive, max.abs.cov)
+
+dsldZlrmOrig <- function(response, predictors sensitive, max.abs.cov)
+{
+  fairml::zlrm.orig(response = response, predictors = predictors, 
+                    sensitive = sensitive, max.abs.cov = max.abs.cov)
+}
+
+#No Examples were provided in fairml.pdf
