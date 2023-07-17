04aff07976b46986b22f9b07dba77b1c6ae906e2
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-07-16 18:43:13 -0700

    Cleanup, added validate_input
    validate_input() in progress

--- inst/Python/dsldFreqPCoord_Py_R.py
+++ inst/Python/dsldFreqPCoord_Py_R.py
@@ -3,7 +3,7 @@
     The code uses rpy2 to handle dsld functions call from R and pandas library to check if
     users data input is in pandas data frame before doing any computation
 '''
-from Utils import dsld_Rpy2_IsRDataframe
+from Utils import dsld_Rpy2_IsRDataframe, R_NULL
 import pandas as pd
 import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
@@ -13,29 +13,53 @@ import os # Image removal
 from PIL import Image # For displaying the graph
 import sys # Cmd line args
 
-# Installing DSLD: must install devtools first since that's how we access dsld during development
-# devtools = importr("devtools")
-
-# This below line may need to be commented
-# Would have to use utils package in order to install dsld from CRAN
-# We used the commented code below to install dsld from our local computer
-#devtools.install_local("/Users/tahaabdullah/Documents/GitHub/dsld")
-
 # dsld package instance. It allows us to call dsld functions inside Python code
 dsld = importr("dsld")
 
-# For handling null arguments
-R_NULL = robjects.NULL
+# This function checks if dsld function arguments are in proper format for computation
+def validate_input(m, columns, sName, method, faceting, k, klm, keepidxs, plotidxs):
+    if type(m) != int:
+        print('Error: m must be an int type. Entered type:', type(m))
+        exit(1)
+
+    if columns != R_NULL and 
+        not all(isinstance(column, str) for column in columns) and
+        not all(isinstance(column, int) for column in columns):
+        print('Error: columns must be a list of strings or ints. Entered type:', type(columns))
+        exit(1)
+
+    if type(sName) != list and type(sName) != str:
+        print('Error: sName must be a list of string. Entered type:', type(sName))
+        exit(1)
+
+    if type(xName) != list and type(xName) != str:
+        print('Error: xName must be a list of string. Entered type:', type(xName))
+        exit(1)
+
+    if type(condits) != list and type(condits) != str:
+        print('Error: condits must be a list of string. Entered type:', type(condits))
+        exit(1)
+
+    if type(useLoess) != bool:
+        print('Error: useLoess must be boolean type. Entered:', type(useLoess))
+        exit(1)
+
+    if yLim is not None and type(yLim) != list:
+        print('Error: yLim must be a list of integers. Entered type:', type(yLim))
+        exit(1)
+    
+    if yLim is not None and len(yLim) != 2:
+        print('Error: yLim must be a list of 2 integers(lower and upper bound). Size Entered:', len(yLim))
+        exit(1)
 
 # dsldFreqPCoord function is called inside this function
 # The arguments are passed inside dsldFreqPCoord as r format
 # and the result is a graph handled by R.
-def dsldPyFreqPCoord(data, m, columns = R_NULL, sName = R_NULL, method = "maxdens", faceting = "vert", k = 50, klm = R_NULL, keepidxs = R_NULL, plotidxs = False, randclrs = False):
+def dsldPyFreqPCoord(data, m, columns = R_NULL, sName = R_NULL, method = "maxdens", faceting = "vert", k = 50, klm = R_NULL, keepidxs = R_NULL, plotidxs = False):
     #************************** ARGUMENTS *******************************************
     r_data = dsld_Rpy2_IsRDataframe(data) # At this point, data is always intended to be in R dataframe format
-    m_r = robjects.IntVector([m])  
+    m_r = robjects.IntVector([m])
 
-    # Columns can be entered in shell as all strings or all ints
     if columns == R_NULL:
         robjects.r.assign("r_data", r_data)  
         robjects.r('columns_r <- 1:ncols(r_data)')
@@ -45,8 +69,6 @@ def dsldPyFreqPCoord(data, m, columns = R_NULL, sName = R_NULL, method = "maxden
             columns_r = robjects.StrVector(columns)
         elif all(isinstance(column, int) for column in columns):
             columns_r = robjects.IntVector(columns)
-        else:
-            print("ERROR: Columns should be either all integers or all strings")
 
     if sName == R_NULL:
         sName_r = sName
@@ -69,7 +91,6 @@ def dsldPyFreqPCoord(data, m, columns = R_NULL, sName = R_NULL, method = "maxden
         keepidxs_r = robjects.IntVector([keepidxs])
     
     plotidxs_r = robjects.BoolVector([plotidxs])
-    randclrs_r = robjects.BoolVector([randclrs])
 
     # All necessary arguments are in R format at this point
     #************************** END ARGUMENTS *******************************************
@@ -79,7 +100,7 @@ def dsldPyFreqPCoord(data, m, columns = R_NULL, sName = R_NULL, method = "maxden
     plot_filename = "freqp_coord.png"
 
     # Calling the R function
-    dsld.dsldFreqPCoord(r_data, m_r, columns_r, sName_r, method_r, faceting_r, k_r, klm_r, keepidxs_r, plotidxs_r, cls_r, plot_filename)
+    dsld.dsldFreqPCoord(r_data, m_r, columns_r, sName_r, method_r, faceting_r, k_r, klm_r, keepidxs_r, plotidxs_r, plot_filename)
 
     # Load and display the saved image in Python
     image = Image.open(plot_filename)
