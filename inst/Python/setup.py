debc86e401bce22ff97a5658aa4240286610a971
Billy <ouattarabilly33@hotmail.com>
2023-07-17 18:53:48 -0700

    Added setup file for making dsld a Python Library

---
+++ inst/Python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='dsld',
+    version='1.0.0',
+    author='T. Abdullah, S. Martha, B. Ouattara, B. Zarate',
+    description='This library serves as a Python interface for R\'s dsld package',
+    packages=['dsld'],
+)
