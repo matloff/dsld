639bc841ace172da762d42c641e3cd26acf6dd26
Arjun Ashok <arjun3.ashok@gmail.com>
2023-07-21 21:36:21 -0700

    skeleton code for categorical auxiliary function, organizational and documentation changes

--- R/dsldConfounders.R
+++ R/dsldConfounders.R
@@ -2,24 +2,48 @@
 #' library(dsld)
 #' data(svcensus)
 #' dsldConfounders(svcensus, "educ")
-dsldConfounders <- function(data, sName = NULL, fill = FALSE) {
+
+
+# ----------------------- Dispatching w/ Confounders ----------------------- #
+#' ::: Description :::
+#' @brief description goes here...
+#'
+#' ::: Arguments :::
+#' @param data: dataset, an R dataframe
+#' @param yName: response variable
+#' @param sName: name of the sensitive variable, an R factor
+#' @param fill: whether or not to fill curve space, an R logical; defaults to
+#'      FALSE, not applicable to categorical data
+#' 
+dsldConfounders <- function(data, yName, sName = NULL, fill = FALSE) {
+    # dispatch to appropriate auxiliary method
     for (i in 1:ncol(data)) {
-        if (is.factor(data[,i])) {
+        # if categorical
+        if (is.factor(data[, i])) {
             print("categorical")
             print(colnames(data)[i])
             # call tabular function here
-        }
-        else if (is.numeric(data[,i])) {
+        # if numeric
+        } else if (is.numeric(data[,i])) {
             print(dsldDensityByS(data,colnames(data)[i],sName,fill))
-        }
-        else {
-            print("Not categorical or numeric")
+        # throw error
+        } else {
+            stop("Neither categorical or numeric column, check dataframe")
         }
     }
 }
 
-#' Graphs densities of a response variable, grouped by a sensitive variable
+
+# ----------------------- Auxiliary for Numeric ----------------------- #
+#' @examples
+#' library(dsld)
+#' data(svcensus)
+#' dsldDensityByS(svcensus)
+#' 
+#' ::: Description :::
+#' @brief Graphs densities of a response variable, grouped by a sensitive variable
 #'
+#' ::: Arguments :::
 #' @param data A dataframe with 1 numerical column and a factor column
 #' @param yName A name or index of the numerical column
 #' @param sName A name or index of the factor column
@@ -27,10 +51,6 @@ dsldConfounders <- function(data, sName = NULL, fill = FALSE) {
 #'
 #' @export
 #'
-#' @examples
-#' library(dsld)
-#' data(svcensus)
-#' dsldDensityByS(svcensus)
 dsldDensityByS <- function(data, yName = NULL, sName = NULL, fill = FALSE) {
     if (is.null(sName))
         sName <- makeSName(data)
@@ -88,6 +108,84 @@ dsldDensityByS <- function(data, yName = NULL, sName = NULL, fill = FALSE) {
         legend = list(title = list(text = sNameStr))
     )
     
-    fig
-        
+    fig       
 }
+
+
+# ----------------------- Auxiliary for Categorical ----------------------- #
+#' @examples
+#' library(dsld)
+#' data(svcensus)
+#' dsldDensityByS(svcensus)
+#' 
+#' ::: Description :::
+#' @brief Extracts frequencies of a response variable, grouped by a sensitive variable
+#'
+#' ::: Arguments :::
+#' @param data A dataframe with 1 numerical column and a factor column
+#' @param yName A name or index of the numerical column
+#' @param sName A name or index of the factor column
+#' @param fill A logical value determining if the graphed curve should be filled in
+#'
+#' @export
+#'
+dsldFrequencyByS <- function(data, yName = NULL, sName = NULL, fill = FALSE) {
+    if (is.null(sName))
+        sName <- makeSName(data)
+    else if (!class(data[, sName]) %in% c("factor", "character"))
+        stop(
+            "sName should be of factor or character data type. Consider setting this as a yName instead"
+        )
+    
+    # for now, if theres no sName, this makes one so the function doesnt break
+    if (is.null(sName)) {
+        Group <- as.factor(rep(1, length(data[, 1])))
+        data <- cbind(data, Group)
+        sName <- length(data)
+    }
+    
+    # yNames <- a vector of 2 ints/strings that correspond to the columns to be used for
+    # the 2 axis on the graph. The user can specify the cols or
+    # yNames will be the first 2 columns that are of numeric or integer data type
+    if (is.null(yName))
+        yName <- makeYNames(data, 1)
+    
+    sGroups <- levels(unique(data[, sName]))
+    
+    yNameStr <- names(data[yName])
+    sNameStr <- names(data[sName])
+    
+    filltype = 'none'
+    if (fill) filltype = 'tozeroy'
+    
+    fig <-
+      plotly::plot_ly(
+        type = 'scatter',
+        mode = 'lines',
+        fill = filltype
+      )
+    
+    for (i in 1:length(sGroups)) {
+        den <- density(data[data[, sName] == sGroups[i],][, yName])
+    
+        fig <-
+            plotly::add_trace(
+                fig,
+                x = den$x,
+                y = den$y,
+                mode = 'lines',
+                name = sGroups[i]
+            )
+    }
+    
+    fig <- plotly::layout(
+        fig,
+        title = paste("Density of", yNameStr, "by", sNameStr),
+        xaxis = list(title = yNameStr),
+        yaxis = list(title = "Density"),
+        legend = list(title = list(text = sNameStr))
+    )
+    
+    fig       
+}
+
