e0ac7a0e7fb837ca3a9ba723cdf3f896b956c923
Billy Tahirou Ouattara <66051428+BillKG-exe@users.noreply.github.com>
2023-07-24 17:26:23 -0700

    Implemented code from EDFfair

--- R/dsldQeFairRF.R
+++ R/dsldQeFairRF.R
@@ -1,10 +1,47 @@
-dsldQeFairRF <- function(data,yName,deweightPars,sName=NULL,
-                     nTree=500,minNodeSize=10,mtry = floor(sqrt(ncol(data))),
-                     yesYVal=NULL,holdout=floor(min(1000,0.1*nrow(data)))) {
-  EDFfair::qeFairRF(data, yName, deweightPars, sensNames = sName, nTree = nTree,
-                    minNodeSize = minNodeSize, mtry = mtry, yesYVal = yesYVal, 
-                    holdout = holdout)
+dsldQeFairRF <- function(data,yName,deweightPars,sensNames=NULL,
+   nTree=500,minNodeSize=10,mtry = floor(sqrt(ncol(data))),
+   yesYVal=NULL,holdout=floor(min(1000,0.1*nrow(data))))
+{
+   require(qeML)
+
+   prepData(1,scaling='none')
+
+   rfout <- qeRFranger(data2,'y',
+      nTree=nTree,minNodeSize=minNodeSize,mtry=mtry,
+      deweightPars,yYesName=yesYVal,holdout=holdout)
+
+   fairRFout <- list(rfout=rfout)
+   fairRFout$classif <- rfout$classif
+   fairRFout$deweightPars <- deweightPars
+   fairRFout$sensNames <- sensNames
+   fairRFout$trainRow1 <- trainRow1
+   fairRFout$factorsInfo <- factorsInfo
+   class(fairRFout) <- c('qeFairRF')
+   fairRFout$holdIdxs <- rfout$holdIdxs
+   fairRFout$holdoutPreds <- rfout$holdoutPreds
+   fairRFout$testAcc <- rfout$testAcc
+   fairRFout$baseAcc <- rfout$baseAcc
+   fairRFout$confusion <- rfout$confusion
+   fairRFout$scaling <- 'none'
+
+   if (!is.null(sensNames) && !is.null(holdout)) {
+      fairRFout$corrs <- corrsens(data,yName,rfout,sensNames)
+      if (fairRFout$classif)
+         fairRFout$sensConfusion <- calcSensConfusion(data,data1,yName,
+            fairRFout$holdIdxs,fairRFout$holdoutPreds,sensNames)
+   }
+
+   fairRFout
 }
+
+predict.dsldQeFairRF <- function(object,newx)
+{
+   newx <- prepNewx(object,newx)
+   rfout <- object$rfout
+   classif <- object$classif
+   predict(rfout,newx)
+}
+ 
 
 # library(dsld)
 # data(svcensus)
