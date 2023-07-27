64c273aefafaa10ca21eb32bb1d401a5aeee629e
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-07-27 16:55:38 -0700

    Added graphType argument and old plot() functionality

--- R/dsldConfounders.R
+++ R/dsldConfounders.R
@@ -14,10 +14,10 @@
 #' @param fill: whether or not to fill curve space, an R logical; defaults to
 #'      FALSE, not applicable to categorical data
 #'
-dsldConfounders <- function(data, sName) {
+dsldConfounders <- function(data, sName, graphType = "plotly", fill = FALSE) {
     # Error checking
     if (is.null(sName)) {
-        stop("sName must be provided as a string of a column name")
+        stop(paste("sName must be provided as a string of a column name"))
     }
 
     # dispatch to appropriate auxiliary method
@@ -29,12 +29,12 @@ dsldConfounders <- function(data, sName) {
             temp_input <- readline()
         # if numeric
         } else if (is.numeric(data[, i])) {
-            print(dsldDensityByS(data, colnames(data)[i], sName))
+            print(dsldDensityByS(data, colnames(data)[i], sName, graphType, fill))
             cat("Press <ENTER> to view next density graph / frequency dataframe...")
             temp_input <- readline()
         # throw error
         } else {
-            stop("Neither categorical or numeric column, check dataframe")
+            stop(paste("Neither categorical or numeric column, check dataframe"))
         }
     }
 }
@@ -57,112 +57,125 @@ dsldConfounders <- function(data, sName) {
 #'
 #' @export
 #'
-dsldDensityByS <- function(data, cName = NULL, sName = NULL) {
-  if (is.null(sName))
-    sName <- makeSName(data)
-  else if (!class(data[, sName]) %in% c("factor", "character"))
-    stop(
-      "sName should be of factor or character data type. Consider setting this as a cName instead"
-    )
-  
-  # for now, if theres no sName, this makes one so the function doesnt break
-  if (is.null(sName)) {
-    Group <- as.factor(rep(1, length(data[, 1])))
-    data <- cbind(data, Group)
-    sName <- length(data)
-  }
-  
-  # cNames <- a vector of 2 ints/strings that correspond to the columns to be used for
-  # the 2 axis on the graph. The user can specify the cols or
-  # cNames will be the first 2 columns that are of numeric or integer data type
-  if (is.null(cName))
-    cName <- makeyNames(data, 1)
-  
-  numGroups <- length(levels(unique(data[, sName])))
-  
-  cNameStr <- names(data[cName])
-  sNameStr <- names(data[sName])
-  
-  bw <- seq(.25, 4, .25)
-  
-  # aval <- a list of the arguements of all the lines we're going to graph
-  aval <- list()
-  for (i in 1:length(bw)) {
-    # from plotly: creating a single group-separated density dataframe object to graph
-    dens <-
-      with(data, tapply(data[, cName], INDEX = data[, sName], density, adjust = bw[i]))
-    df <- data.frame(
-      x = unlist(lapply(dens, "[[", "x")),
-      y = unlist(lapply(dens, "[[", "y")),
-      group = rep(names(dens), each = length(dens[[1]]$x))
-    )
+dsldDensityByS <- function(data, cName, sName, graphType = "plotly", fill = FALSE) {
+    if (!class(data[, sName]) %in% c("factor", "character"))
+        stop(paste("sName should be of factor or character data type. Consider setting this as a cName instead"))
+
+    numGroups <- length(levels(unique(data[, sName])))
     
-    aval[[i]] <- list(visible = FALSE,
-                      x =  df$x,
-                      y =  df$y)
-  }
-  
-  aval[[4]]$visible = TRUE
-  
-  steps <- list()
-  fig <- plotly::plot_ly(type = 'scatter',
-                         mode = 'lines',
-                         color = df$group)
-  for (i in 1:length(bw)) {
-    fig <-
-      plotly::add_lines(fig,
-                        x = aval[[i]]$x,
-                        y = aval[[i]]$y,
-                        visible = aval[[i]]$visible)
-    # if there are 3 groups in sName, and there are 8 bandwidths, we need to initally
-    # set all 24 graphs's visibility to false
-    step <-
-      list(
-        args = list('visible', rep(FALSE, length(aval) * numGroups)),
-        method = 'restyle',
-        label = bw[i]
-      )
-    # and then the correct 3 to true
-    step$args[[2]][1:numGroups + numGroups * i] <- TRUE
-    steps[[i]] <- step
-  }
-  
-  buttons <- list(
-    list(
-      method = "restyle",
-      args = list("fill", "none"),
-      label = "no fill"
-    ),
-    list(
-      method = "restyle",
-      args = list("fill", "tozeroy"),
-      label = "fill"
-    )
-  )
-  
-  # add slider control to plot
-  fig <-
-    plotly::layout(
-      fig,
-      updatemenus = list(list(
-        active = 0,
-        x = 0,
-        y = 1,
-        buttons = buttons
-      )),
-      sliders = list(list(
-        active = 3,
-        currentvalue = list(prefix = "Adjust: "),
-        steps = steps
-      )),
-      title = paste("Density of", cNameStr, "by", sNameStr),
-      xaxis = list(title = cNameStr),
-      yaxis = list(title = "Density"),
-      legend = list(title = list(text = sNameStr))
-    )
-  
-  
-  fig
+    cNameStr <- names(data[cName])
+    sNameStr <- names(data[sName])
+    
+    bw <- seq(.25, 4, .25)
+    
+    if (tolower(graphType) == "plot") {
+        # ************ plot() *********************************
+        sGroups <- levels(unique(data[, sName]))
+        for (i in 1:length(sGroups)) {
+            den <- density(data[data[, sName] == sGroups[i], ][, cName])
+
+            if (i == 1)
+                plot(
+                    den,
+                    col = i,
+                    xlab = cNameStr,
+                    main = paste("Density of", cNameStr, "by", sNameStr)
+                )
+            else
+                lines(den, col = i)
+
+            if (fill)
+                polygon(den, col = i)
+        }
+
+        legend(
+            "topright",
+            title = sNameStr,
+            legend = sGroups,
+            col = 1:length(sGroups),
+            lty = 1
+        )
+        # ************ plot() *********************************
+    } else if (tolower(graphType) == "plotly") {
+        # ************ plotly *********************************
+        # aval <- a list of the arguements of all the lines we're going to graph
+        aval <- list()
+        for (i in 1:length(bw)) {
+            # from plotly: creating a single group-separated density dataframe object to graph
+            dens <-
+            with(data, tapply(data[, cName], INDEX = data[, sName], density, adjust = bw[i]))
+            df <- data.frame(
+            x = unlist(lapply(dens, "[[", "x")),
+            y = unlist(lapply(dens, "[[", "y")),
+            group = rep(names(dens), each = length(dens[[1]]$x))
+            )
+            
+            aval[[i]] <- list(visible = FALSE,
+                            x =  df$x,
+                            y =  df$y)
+        }
+        
+        aval[[4]]$visible = TRUE
+        
+        steps <- list()
+        fig <- plotly::plot_ly(type = 'scatter',
+                                mode = 'lines',
+                                color = df$group)
+        for (i in 1:length(bw)) {
+            fig <-
+            plotly::add_lines(fig,
+                                x = aval[[i]]$x,
+                                y = aval[[i]]$y,
+                                visible = aval[[i]]$visible)
+            # if there are 3 groups in sName, and there are 8 bandwidths, we need to initally
+            # set all 24 graphs's visibility to false
+            step <-
+            list(
+                args = list('visible', rep(FALSE, length(aval) * numGroups)),
+                method = 'restyle',
+                label = bw[i]
+            )
+            # and then the correct 3 to true
+            step$args[[2]][1:numGroups + numGroups * i] <- TRUE
+            steps[[i]] <- step
+        }
+        
+        buttons <- list(
+            list(
+            method = "restyle",
+            args = list("fill", "none"),
+            label = "no fill"
+            ),
+            list(
+            method = "restyle",
+            args = list("fill", "tozeroy"),
+            label = "fill"
+            )
+        )
+        
+        # add slider control to plot
+        fig <-
+            plotly::layout(
+            fig,
+            updatemenus = list(list(
+                active = 0,
+                x = 0,
+                y = 1,
+                buttons = buttons
+            )),
+            sliders = list(list(
+                active = 3,
+                currentvalue = list(prefix = "Adjust: "),
+                steps = steps
+            )),
+            title = paste("Density of", cNameStr, "by", sNameStr),
+            xaxis = list(title = cNameStr),
+            yaxis = list(title = "Density"),
+            legend = list(title = list(text = sNameStr))
+            )
+        fig
+        # ************ plotly *********************************
+    }
 }
 
 # ----------------------- Auxiliary for Categorical ----------------------- #
@@ -182,7 +195,7 @@ dsldDensityByS <- function(data, cName = NULL, sName = NULL) {
 #'
 #' @export
 #'
-dsldFrequencyByS <- function(data, cName = NULL, sName = NULL) {
+dsldFrequencyByS <- function(data, cName, sName) {
     comment <- "
     # ensure libraries #
     getSuggestedLib('gt')
@@ -193,47 +206,12 @@ dsldFrequencyByS <- function(data, cName = NULL, sName = NULL) {
         gt()
     "
 
-    # force missing vars #
-    # check sensitive variable is missing
     if (!class(data[, sName]) %in% c("factor", "character")) {
         stop(paste(
             "sName should be of factor or character data type. Consider",
             " setting this as a cName instead"
         ))
     }
-    
-    # check missing response variable
-    if (is.null(cName)) {
-        stop("null cName")
-    }
-
-
-    # -------- Iterative Approach I -------- #
-    # # sensitive variable frequencies #
-    # # divide by level
-    # yGroups <- levels(factor(data[[cName]]))
-    # sGroups <- levels(factor(data[[sName]]))
-
-    # # setup dataframe
-    # frequencies <- data.frame(matrix(NA, nrow = length(sGroups),
-    #     ncol = length(yGroups)))
-    # rownames(frequencies) <- sGroups
-    # colnames(frequencies) <- yGroups
-    
-    # # find frequencies for each level
-    # for (s in seq_along(sGroups)) {
-    #     # for each response level
-    #     for (y in seq_along(yGroups)) {
-    #         # add frequency
-    #         freq <- sum(data[[sName]] == sGroups[s] &&
-    #             data[[cName]] == yGroups[y])
-    #         frequencies[s, y] <- freq
-    #     }
-    # }
-
-    # # return frequencies #
-    # return(frequencies)
-
 
     # -------- Efficient Approach II -------- #
     # sensitive variable frequencies #
@@ -251,15 +229,6 @@ dsldFrequencyByS <- function(data, cName = NULL, sName = NULL) {
         # sNameStr,
         paste0("Frequency of ", yGroups)
     )
-
-    # # merge levels of sName into one row
-    # uniqueSGroups <- data.frame(sName = unique(data[[sName]]))
-    # names(uniqueSGroups) <- c(sNameStr)
-    # frequencies <- merge(uniqueSGroups, frequencies, by = sNameStr,
-    #     all.x = TRUE)
-
-    # # fill missing combination values with 0
-    # frequencies[is.na(frequencies)] <- 0
 
     # return frequncies #
     return(frequencies)
