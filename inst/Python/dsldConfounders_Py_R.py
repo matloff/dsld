d8a94b7c5348a66df695eef93f82d28cc79f360e
Taha Abdullah <106115454+tmabdull@users.noreply.github.com>
2023-07-27 17:19:52 -0700

    Added plot() save graph as img
    Only saves the last image

--- inst/Python/dsldConfounders_Py_R.py
+++ inst/Python/dsldConfounders_Py_R.py
@@ -7,8 +7,11 @@ Ipython.display is used to display the interactive scatter plot
 sys is used for OS shell
 '''
 
-from Utils import dsld_Rpy2_IsRDataframe, R_NULL, ERROR
+from Utils import dsld_Rpy2_IsRDataframe, changeBg, R_NULL, ERROR
+from PIL import Image
+import os
 from rpy2.robjects.packages import importr
+from rpy2.robjects import r
 from IPython.display import display
 import rpy2.robjects as robjects
 import pandas as pd
@@ -23,8 +26,9 @@ rplotly is used to create a plotly widget to aid in displaying graph
 '''
 dsld    = importr("dsld")
 rplotly = importr("plotly")
+grdevices = importr('grDevices')
 
-def dsldPyConfounders(data, sName):
+def dsldPyConfounders(data, sName, graphType = "plotly", fill = False):
     # ************************** ARGUMENTS *******************************************
     
     # Data conversion handled by Utils function
@@ -50,23 +54,48 @@ def dsldPyConfounders(data, sName):
             print('Error: sName needs to be a string or integer. It cannot be null')
             exit(ERROR)
 
-    result = dsld.dsldConfounders(r_data, sName_r)
+    graphType_r = robjects.StrVector([graphType])
+
+    fill_r = robjects.BoolVector([fill])
     
     # All necessary arguments are in R format at this point
     # ************************** END ARGUMENTS *******************************************
     
     # ************************** RETURN VALUE *******************************************
-    # Convert the plot to a Plotly widget and display it
-    plot_widget = rplotly.as_widget(result)
-    display(plot_widget)
+    if graphType.lower() == "plotly":
+        result = dsld.dsldConfounders(r_data, sName_r, graphType_r, fill_r)
+        # Convert the plot to a Plotly widget and display it
+        plot_widget = rplotly.as_widget(result)
+        display(plot_widget)
+    elif graphType.lower() == "plot":
+        # TODO: graph saved as img
+        print("******************* PLOT ********************")
+        dsld.dsldConfounders(r_data, sName_r, graphType_r, fill_r)
+        # Copy and saves the image as confounders_plot.png
+        plot_filename = "confounders_plot.png"
+        grdevices.dev_copy(device=r.png, filename=plot_filename)
+        grdevices.dev_off()
+
+        # Set background of image saved to white
+        changeBg(plot_filename)
+
+        # Load the image file in Python
+        image = Image.open(plot_filename)
+        image.show() # Display the plot using the default image viewer
+
+        # Close the displayed image
+        image.close()
+        # Delete the image file
+        os.remove(plot_filename)
+
     # ************************** END FUNCTION *******************************************
 
 #************************** OS SHELL FUNCTIONALITY *************************************
 # TODO: OS Shell functionality is INCOMPLETE
 if __name__ == "__main__":
-    cmd, data, sName = sys.argv
+    cmd, data, sName, graphType, fill = sys.argv
     data = pd.read_csv()
-    dsldPyConfounders(data, sName)
+    dsldPyConfounders(data, sName, graphType, bool(fill))
 #************************** END OS SHELL *******************************************
 
 '''
