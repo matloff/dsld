7be0a47f8a0b8d135423eb200a19d51c01cfddd5
adimittal03 <129902929+adimittal03@users.noreply.github.com>
2023-07-29 16:44:54 -0700

    Update dsldQeFairRF.R

--- R/dsldQeFairRF.R
+++ R/dsldQeFairRF.R
@@ -24,6 +24,7 @@ dsldQeFairRF <- function(data,yName,deweightPars,sensNames=NULL,
                      nTree=500,minNodeSize=10,mtry = floor(sqrt(ncol(data))),
                      yesYVal=NULL,holdout=floor(min(1000,0.1*nrow(data))))
 {
+  if (!require('ranger')) install.packages('ranger'); library('ranger')
   require(qeML)
   
   prepData(1,scaling='none')
