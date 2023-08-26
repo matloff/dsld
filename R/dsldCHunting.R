ce3adba6c0c9abe89ad99dd5623bc0d663099e74
Norm Matloff <nsmatloff@ucdavis.edu>
2023-08-26 13:59:18 -0700

    for confounder selection

--- R/dsldCHunting.R
+++ R/dsldCHunting.R
@@ -12,9 +12,9 @@ dsldCHunting <- function(data,yName,sName)
 {
 
    ycol <- which(names(data) == yName)
+   scol <- which(names(data) == sName)
    y <- data[,ycol]
    s <- data[,scol]
-   scol <- which(names(data) == sName)
    dataNoS <- data[,-scol]  # for predicting Y
    dataNoY <- data[,-ycol]  # for predicting S
 
@@ -25,5 +25,16 @@ dsldCHunting <- function(data,yName,sName)
          stop('Y must be numeric or an R factor')
    if (is.factor(s)) impS1 <- impS[,length(levels(s))+1] else 
          stop('S must be numeric or an R factor')
+   impY1 <- sort(impY1,decreasing=TRUE)
+   impS1 <- sort(impS1,decreasing=TRUE)
+   res <- list(impForY=impY1,impForS=impS1)
+   nmsY <- names(impY1)
+   nmsS <- names(impS1)
+   res$inCommon <- list()
+   for (i in 1:min(10,ncol(data)-2)) {
+      res$inCommon[[i]] <- intersect(nmsY[1:i],nmsS[1:i])
+   }
+   res
+
 
 }
