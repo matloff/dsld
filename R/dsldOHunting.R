73c6642b178ec0c808335a9f94c1269c0b3fbd0f
Norm Matloff <nsmatloff@ucdavis.edu>
2023-09-08 10:45:01 -0700

    new proxy hunter

---
+++ R/dsldOHunting.R
@@ -0,0 +1,23 @@
+
+# ad hoc aid in deciding which covariates one should treat as
+# proxies
+
+# we want to find variables O that are correlated with S; S need not be
+# binary/categorical
+
+# based on cor(), using Kendall's Tau in order to acccomdate binary
+# variables (0,1 valued), and to mitigate effects of outliers
+
+dsldOHunting <- function(data,yName,sName) 
+{
+
+   ycol <- which(names(data) == yName)
+   scol <- which(names(data) == sName)
+
+   sdumms <- regtools::factorsToDummies(data[,scol])
+   odumms <- regtools::factorsToDummies(data[,-c(ycol,scol)])
+
+   cor(sdumms,odumms,method='kendall')
+
+}
+
