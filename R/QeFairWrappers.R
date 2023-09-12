89de4436f1abc229fddd1395b1c3aac614116663
jotyot <104177975+jotyot@users.noreply.github.com>
2023-09-12 15:27:10 -0700

    parameters standardized

--- R/QeFairWrappers.R
+++ R/QeFairWrappers.R
@@ -11,16 +11,11 @@
 #
 # qeFUNC        - a qeML model generation function
 # data          - unmodified data to train the model on
-# appendedItems - list of items to be appended to the model object.
-#                 must have yName, sNames, scaling.
 # ...           - additional parameters that are passed into the qeFUNC
+# appendedItems - list of items to be appended to the model object.
 #
-qeFairBase <- function(qeFUNC, data, appendedItems, ...) {
-  # extract important variables
-  yName <- appendedItems$yName
-  sNames <- appendedItems$sNames
-  scaling <- appendedItems$scaling
-  
+qeFairBase <- function(qeFUNC, data, yName, sNames, scaling, ..., appendedItems=list()) {
+
   # scale data / expand factors, before training the model
   train <- fairScale(data, yName, sNames, scaling)
   base <- qeFUNC(train, yName, ...)
@@ -51,14 +46,15 @@ dsldQeFairRF <- function(data,yName,sNames,deweightPars=NULL, nTree=500,
                          minNodeSize=10, mtry = floor(sqrt(ncol(data))),
                          yesYVal=NULL,holdout=floor(min(1000,0.1*nrow(data)))) {
   scaling <- FALSE 
-  appendedItems <- variablesAsList(sNames, yName, deweightPars, scaling)
+  appendedItems <- variablesAsList(yName, sNames, scaling, deweightPars)
   
   # expand deweights to match the expanded factors in the data in qeFairBase
   expandedDW <- expandDeweights(deweightPars, data[1,])
-  model <- qeFairBase(qeML::qeRFranger, data, appendedItems, 
+  model <- qeFairBase(qeML::qeRFranger, data, yName, sNames, scaling, 
                       
                       nTree=nTree, minNodeSize=minNodeSize, mtry=mtry,
-                      yesYVal=yesYVal, holdout=holdout, deweightPars = expandedDW)
+                      yesYVal=yesYVal, holdout=holdout, deweightPars = expandedDW,
+                      appendedItems = appendedItems)
   model
 }
 
@@ -74,16 +70,17 @@ dsldQeFairKNN <- function(data, yName, sNames, deweightPars=NULL,
                           yesYVal=NULL,k=25,scaleX=TRUE,
                           holdout=floor(min(1000,0.1*nrow(data)))) {
   scaling <- scaleX
-  appendedItems <- variablesAsList(sNames, yName, deweightPars, scaling)
+  appendedItems <- variablesAsList(yName, sNames, scaling, deweightPars)
   # expand deweights to match the expanded factors in the data in qeFairBase
   expandedDW <- expandDeweights(deweightPars, data[1,])
   expandVars <- names(expandedDW) 
   expandVals <- unlist(expandedDW) # to use as qeKNN pars
   
-  model <- qeFairBase(qeML::qeKNN, data, appendedItems, 
+  model <- qeFairBase(qeML::qeKNN, data, yName, sNames, scaling, 
                       
                       k=k, yesYVal=yesYVal, holdout=holdout,
-                       expandVars=expandVars, expandVals=expandVals)
+                       expandVars=expandVars, expandVals=expandVals,
+                      appendedItems = appendedItems)
   model
 }
 
@@ -91,7 +88,7 @@ dsldQeFairKNN <- function(data, yName, sNames, deweightPars=NULL,
 # predict.dsldQeFair(knn, fairml::compas[1,])
 
 # --------------- QeFairRidgeBase -------------
-
+#
 # base function for the ridge models. 
 # Creates its own training and testing sets 
 # training set is modified w/ ridgeModify, according to EDF paper
@@ -99,27 +96,19 @@ dsldQeFairKNN <- function(data, yName, sNames, deweightPars=NULL,
 # appends additional variables via variables as list
 # appends testAcc, baseAcc, holdoutPreds with the testing set via predictHoldoutFair
 #
-# linear - a logical value. if false, use linear model- if true, use logistic.
-#
-qeFairRidgeBase <- function(linear, data, yName, sNames, deweightPars, 
-                            holdout, yesYVal) {
-  # data w/o sName and scaled xNames. save scalePars for predict()
-  scaling <- TRUE
-  scaledData <- fairScale(data, yName, sNames, scaling)
-  scalePars <- attr(scaledData, 'scalePars')
+qeFairRidgeBase <- function(data, yName, sNames, deweightPars, 
+                            holdout, yesYVal=0) {
+  linear <- yesYVal == 0     # if we're using the default yesYVal, assume its linear
 
-  # test and training sets to use in testAcc section
   if (!is.null(holdout)){
-    holdIdxs <- sample(1:nrow(scaledData),holdout);
+    holdIdxs <- sample(1:nrow(data),holdout);
     test <- data[holdIdxs,];
-    train <- scaledData[-holdIdxs,];
-  } else train <- scaledData
+    train <- data[-holdIdxs,];
+  } else train <- data
   
   # perform formula described in edffair paper
-  expandDW <- expandDeweights(deweightPars, data[1,])
-  # in linear case: 0- in nonlinear: the no value
-  yBlank <- if (linear) 0 else setdiff(levels(data[,yName]), yesYVal)
-  dataExtended <- ridgeModify(train, expandDW, yBlank)
+  dataExtended <- ridgeModify(train, yName, sNames, deweightPars, yesYVal)
+  scalePars <- attr(dataExtended, 'scalePars')
   
   base <- 
     if (linear) qeML::qeLin(dataExtended,yName,holdout=NULL)
@@ -127,9 +116,9 @@ qeFairRidgeBase <- function(linear, data, yName, sNames, deweightPars,
 
   # Append these variables to the model object
   classif <- !linear
+  scaling <- TRUE
   appendedItems <- variablesAsList(
-    sNames, yName, deweightPars, scaling, scalePars, classif)
-  if (!linear) appendedItems$yesYVal <- yesYVal
+    sNames, yName, deweightPars, scaling, scalePars, classif, yesYVal)
   
   # construct output model object
   model <- list(base=base)
@@ -156,35 +145,38 @@ qeFairRidgeBase <- function(linear, data, yName, sNames, deweightPars,
 # with each value of the diagonal optionally having a deweighting value
 # the yCol is expanded w/ 0s or a no 
 # 
-# train    - expanded factor + scaled training data
-# expandDW - expanded factor deweights
-# yBlank   - value to append to the y column to expand it. 0 in the linear case
-#            the no value in the general case
+# yesYVal - the yes value in the binary yName. if 0, we're assuming its a linear model
 #
-ridgeModify <- function(train, expandDW, yBlank=0) {
+ridgeModify <- function(data, yName, sNames, deweightPars, yesYVal=0) {
+  expandDW <- expandDeweights(deweightPars, data[1,])
   expandVars <- names(expandDW)
   expandVals <- unlist(expandDW)
   
-  xNames <- colnames(train[,-ncol(train)])
-  p <- ncol(train) - 1    # common length for how many predictors there are
+  data <- fairScale(data, yName, sNames, scaling = TRUE)
+  
+  xNames <- colnames(data[,-ncol(data)])
+  noYVal <- setdiff(levels(data[,yName]), yesYVal)[[1]] 
+  yBlank <- if (yesYVal == 0) 0 else noYVal
+  p <- ncol(data) - 1   # how many predictors
   
   # formula described in edffair paper
   D <- setNames(rep(0, p), xNames)
-  if (length(expandDW))
+  if (!is.null(deweightPars))
     D[expandVars] <- sqrt(expandVals)
   
   extension <- data.frame(diag(D))
   extension <- cbind(extension, rep(yBlank, p))
   
-  names(extension) <- colnames(train)
-  dataExtended <- rbind(train, extension)
+  names(extension) <- colnames(data)
+  dataExtended <- rbind(data, extension)
   
+  attr(dataExtended, 'scalePars') <- attr(data, 'scalePars')
   dataExtended
 }
 
 dsldQeFairRidgeLin <- function(data, yName, sNames, deweightPars = NULL, 
                                       holdout=floor(min(1000,0.1*nrow(data)))) {
-  qeFairRidgeBase(linear=TRUE, data, yName, sNames, deweightPars, holdout)
+  qeFairRidgeBase(data, yName, sNames, deweightPars, holdout)
 }
 # lin <- dsldQeFairRidgeLin(svcensus, "wageinc", "gender", deweightPars = list(occ=.4, age=.2))
 # predict.dsldQeFair(lin, svcensus[1,])
@@ -193,7 +185,7 @@ dsldQeFairRidgeLin <- function(data, yName, sNames, deweightPars = NULL,
 dsldQeFairRidgeLog <- function(data,yName,sNames,deweightPars=NULL,
                                holdout=floor(min(1000,0.1*nrow(data))),
                                yesYVal=levels(data[,yName])[2]) {
-  qeFairRidgeBase(linear=FALSE, data, yName, sNames, deweightPars, holdout, yesYVal)
+  qeFairRidgeBase(data, yName, sNames, deweightPars, holdout, yesYVal)
 }
 
 # log <- dsldQeFairRidgeLog(fairml::compas, "two_year_recid", "race")
