747e908c31bdb44e94d4f79e25aee1841bef3cf4
Arjun Ashok <arjun3.ashok@gmail.com>
2023-09-12 15:24:41 -0700

    some linting for dsldFairTest

--- R/dsldFairTest.R
+++ R/dsldFairTest.R
@@ -1,5 +1,5 @@
-dsldFairTest <- function(data, yName, sName, modelFunc, metricFunc, 
-                         ..., cutoff = .5, nReps = 1, 
+dsldFairTest <- function(data, yName, sName, modelFunc, metricFunc,
+                         ..., cutoff = .5, nReps = 1,
                          testProportion = 0.3) {
   dsld::getSuggestedLib("R.utils")
   
@@ -7,7 +7,7 @@ dsldFairTest <- function(data, yName, sName, modelFunc, metricFunc,
     # partition the data
     partition <- sample(1:nrow(data))[1:round(nrow(data) * testProportion)]
     train <- data[-partition, ]
-    test <- data[ partition, ]
+    test <- data[partition, ]
     
     # avoid warning statements since these are used in the book
     sink(nullfile())
@@ -17,7 +17,7 @@ dsldFairTest <- function(data, yName, sName, modelFunc, metricFunc,
       # right now only tested to work with dsldQeFairRF and qeRFranger
       # doCall used to work with unused args
       R.utils::doCall(
-        modelFunc, data=train, yName=yName, sName=sName,
+        modelFunc, data = train, yName = yName, sName = sName,
         ...
       ),
       finally = sink()
@@ -28,38 +28,47 @@ dsldFairTest <- function(data, yName, sName, modelFunc, metricFunc,
   
     # EDFfair wrapper models have a probs attribute
     if ("probs" %in% names(prediction)) {
-      test$probs <- prediction$probs[,1]
-      preds <- (prediction$probs[,1] > cutoff) + 1 # used for misclass error
-    } 
+      test$probs <- prediction$probs[, 1]
+      preds <- (prediction$probs[, 1] > cutoff) + 1 # used for misclass error
+    }
     # fairml wrapper models dont
     else {
       test$probs <- prediction
       preds <- (prediction > cutoff) + 1 # used for misclass error
     }
     
-    # perform fairness function Metric or 
+    # perform fairness function Metric or
     # user passed function with access to the model
     Metric <- R.utils::doCall(
-      metricFunc, data=test, outcome=yName, group=sName, probs='probs', cutoff=cutoff,
-        model=model
-      )
+      metricFunc,
+      data = test,
+      outcome = yName,
+      group = sName,
+      probs = "probs",
+      cutoff = cutoff,
+      model = model
+    )
+
     # fairness object has a Metric column
-    if ("Metric" %in% names(Metric)) Metric <- t(Metric$Metric)
+    if ("Metric" %in% names(Metric)) {
+      Metric <- t(Metric$Metric)
+    }
     
     # manually calculated test accuracy
-    actual <- as.numeric(test[,yName])
+    actual <- as.numeric(test[, yName])
     error <- mean(preds != actual)
     
     # append test accuracy to the output
     Metric <- cbind(Metric, NA)
     Metric[1, ncol(Metric)] <- error
     colnames(Metric)[ncol(Metric)] <- "Misclass Error"
-    Metric
+    
+    return(Metric)
   }
   
   # repeats the calculation nreps times and averages the data tables
   suppressWarnings({
-    apply(replicate(nReps, singleTest()), c(1,2), mean, na.rm=TRUE)
+    apply(replicate(nReps, singleTest()), c(1, 2), mean, na.rm = TRUE)
   })
 }
 
