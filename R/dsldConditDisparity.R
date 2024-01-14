76a629f01dfb1804078056a6d0c1d7722f5c20b4
Arjun Ashok <arjun3.ashok@gmail.com>
2024-01-14 10:32:03 -0800

    changes to condit disparity for package checking

--- R/dsldConditDisparity.R
+++ R/dsldConditDisparity.R
@@ -2,7 +2,7 @@
 # arguments
 
 #   data: input data frame or equivalewnt
-#   yName: response variable 
+#   yName: response variable
 #   sName: sensitive variable (R factor)
 #   xName: horizontal axis variables
 #   condits: conditions, a vector of conditions, expressed in
@@ -13,19 +13,22 @@
 #   useLoess: if TRUE, use loess smoothing
 
 dsldConditDisparity <- function(data, yName, sName, xName, condits,
-                                qeFtn = qeKNN, minS = 50, useLoess = TRUE) 
+                                qeFtn = qeKNN, minS = 50, useLoess = TRUE)
 {
     # args type checking
     if (!is.data.frame(data)) {
         stop("data must be a dataframe.")
     }
+
     y <- data[[yName]]
-    dichotY <- inherits(y,'factor') && length(levels(y)==2)
-    if (!inherits(y,'numeric') &&
-        !inherits(y,'integer') &&
+    dichotY <- inherits(y, "factor") && length(levels(y) == 2)
+
+    if (!inherits(y, "numeric") &&
+        !inherits(y, "integer") &&
         !dichotY
-       )
+       ) {
         stop("yName must refer to a numeric or 2-level factor column in data.")
+    }
     if (!is.factor(data[[sName]])) {
         stop("sName must refer to a factor column in data.")
     }
@@ -33,15 +36,15 @@ dsldConditDisparity <- function(data, yName, sName, xName, condits,
         stop("xName must refer to a numeric column in data.")
     }
 
-    require(qeML)
+    getSuggestedLib("qeML")
 
     # data engineering #
     # restrict data to fit conditions
     if (length(condits) > 1) {
         # combine conditions
-        condits <- paste(condits, collapse = ' & ')
+        condits <- paste(condits, collapse = " & ")
     }
-    restrictions <- sprintf('focusedData <- subset(data, %s)', condits)
+    restrictions <- sprintf("focusedData <- subset(data, %s)", condits)
     eval(parse(text = restrictions))
     focusedData <- focusedData[c(yName, xName, sName)]
     sCol <- which(names(focusedData) == sName)
@@ -51,13 +54,11 @@ dsldConditDisparity <- function(data, yName, sName, xName, condits,
     groupByS <- split(focusedData, s)
     sizes <- sapply(groupByS, nrow)
     tiny <- which(sizes < minS)
+
     # remove too-small groups
-    if (length(tiny) > 0)
-    {
+    if (length(tiny) > 0) {
         groupByS <- groupByS[-tiny]
     }
-
-    # plotting 
 
     # consider only the remaining S-levels
     sLevels <- names(groupByS)
@@ -73,11 +74,11 @@ dsldConditDisparity <- function(data, yName, sName, xName, condits,
        # setup data for training
        curData <- groupByS[[i]][,-sCol]  # current s-level w/o sensitive column
        curXData <- unique(curData[[xName]]) # only the numeric x column
-       curXDF <- as.data.frame(curXData)  
+       curXDF <- as.data.frame(curXData)
        names(curXDF) <- xName  # adjust column name
 
        # fit ML model
-       model <- qeFtn(curData, yName, holdout=NULL) 
+       model <- qeFtn(curData, yName, holdout = NULL)
        preds <- predict(model, curXDF)
        if (dichotY) preds <- preds$probs
 
@@ -102,33 +103,40 @@ dsldConditDisparity <- function(data, yName, sName, xName, condits,
 
    # create plot
    colors <- rainbow(remainingS)
-   predsMax <- max(sapply(predsList,max))
-   predsMin <- min(sapply(predsList,min))
-   ylow = if (predsMin >= 0) 0.9*predsMin else 1.1*predsMin
-   yhigh = if (predsMax >= 0) 1.1*predsMax else 0.9*predsMax
-   currXMax <- max(sapply(curXData,max))
-   currXMin <- min(sapply(curXData,min))
-   plot(NULL,
-       ylim = c(ylow,yhigh),
-       xlim = c(currXMin,currXMax),
-       xlab = xName,
-       ylab = yName,
-       main = paste("Underlying Effects of ", sName, " on ", 
-          yName, " wrt ", xName)
+   predsMax <- max(sapply(predsList, max))
+   predsMin <- min(sapply(predsList, min))
+   ylow <- if (predsMin >= 0) 0.9 * predsMin else 1.1 * predsMin
+   yhigh <- if (predsMax >= 0) 1.1 * predsMax else 0.9 * predsMax
+   currXMax <- max(sapply(curXData, max))
+   currXMin <- min(sapply(curXData, min))
+
+   plot(
+        NULL,
+        ylim = c(ylow, yhigh),
+        xlim = c(currXMin, currXMax),
+        xlab = xName,
+        ylab = yName,
+        main = paste("Underlying Effects of ", sName, " on ", 
+            yName, " wrt ", xName)
    )
 
    for (i in 1:remainingS) {
-      lines(curXDataList[[i]],predsList[[i]],type="l",lty="solid",col=colors[i])
+      lines(
+        curXDataList[[i]],
+        predsList[[i]],
+        type = "l",
+        lty = "solid",
+        col = colors[i]
+      )
    }
 
    legend(
        x = "topright",
-       lty = rep(1,remainingS),
+       lty = rep(1, remainingS),
        text.font = 4,
        col = colors,
        text.col = "black",
        legend = sLevels
    )
 }
-
 
