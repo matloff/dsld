cc86077e3c4d0a81e4c697e4c993e23b282c9404
Aditya Mittal <adimittal@ucdavis.edu>
2024-03-30 21:44:54 -0700

    Update dsldLinear.R

--- R/dsldLinear.R
+++ R/dsldLinear.R
@@ -72,7 +72,7 @@ dsldLinear <- function(data, yName, sName, interactions = FALSE,
     
     # branch covariance matrix on sandwich
     if (useSandwich) {
-      covMatrix <- sandwich(diffModel)
+      covMatrix <- sandwich::sandwich(diffModel)
     } else {
       covMatrix <- vcov(diffModel)
     }
