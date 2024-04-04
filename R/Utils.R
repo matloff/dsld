9010f1d82569f13c15a41314b9d0920f79ba36e1
Aditya Mittal <adimittal@ucdavis.edu>
2024-04-04 12:41:40 -0700

    Update Utils.R

--- R/Utils.R
+++ R/Utils.R
@@ -12,22 +12,6 @@ getSuggestedLib <- function(pkgName) {
       stop(paste0(pkgName, ' not loaded'))
 }
 
-checkPkgLoaded <- function(pkgName,whereObtain='CRAN') 
-{
-      # workaround
-      cmd <- sprintf('%s <- NULL',pkgName)
-      evalr(cmd)
-   
-      cmd <- sprintf('require(%s)',pkgName)
-      if (!evalr(cmd)) {
-         pkgErr <- sprintf('%s is required',pkgName)
-         print(pkgErr)
-         obtain <- paste0('if not installed, obtain from ',whereObtain)
-         stop(sprintf('if not installed, obtain from %s',whereObtain))
-      }
-      requireNamespace(pkgName)
-}
-
 pr2file <- function(filename)
 {
    origdev <- dev.cur()
@@ -55,5 +39,20 @@ cartFactorLvls <- function(factorNames)
 {
    theLevels <- lapply(factorNames,function(fName) levels(get(fName)))
    expand.grid(theLevels)
+}
+
+### --------------------------- dsldGetRow5 ------------------------------------
+### selects 5 rows for comparison across each level of the sensitive variable
+### randomly if the user doesn't supply data in the interactions case
+### USEFUL FOR: dsldLinear, dsldLogit
+dsldGetRow5 <- function(data, yName, sName) {
+  rows <- sample(nrow(data), 5)
+  reducedData <- data[rows, ]
+  columns <- c(yName, sName)
+  newDat <- reducedData[, !(names(reducedData) %in% columns)]
+  result <- sprintf("No user sComparisonPts supplied. The following rows 
+                    are selected: %s,%s,%s,%s,%s", rows[1],rows[2],rows[3],rows[4],
+                    rows[5]); print(result)
+  return(newDat)
 }
 
