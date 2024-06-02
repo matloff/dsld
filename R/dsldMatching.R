4c165c37664736b003f660a5faadc3e259a06719
Norm Matlof <nsmatloff@ucdavis.edu>
2024-06-02 16:50:31 -0700

    rough version ready

---
+++ R/dsldMatching.R
@@ -0,0 +1,44 @@
+
+# dsld wrapper for Matchinf::Match; optional propensFtn must be either
+# 'lm' or 'glm' logit
+
+dsldMatchedATE <- function(data,yName,sName,yesSVal,
+   propensFtn=NULL,yesYVal=NULL) 
+{
+   getSuggestedLib("Matching")
+
+   ycol <- which(names(data) == yName)
+   y <- data[,ycol]
+
+   if (is.factor(y)) {
+      yLvls <- levels(y)
+      if (length(yLvls) != 2)
+         stop('factor Y can only be dichotomous')
+      if (is.null(yesYVal))
+         stop('must specify yesYVal')
+      y <- as.integer(y == yesYVal)
+      dichotY <- TRUE
+   } else dichotY <- FALSE
+
+   scol <- which(names(data) == sName)
+   s <- data[,scol]
+   s <- as.integer(s == yesSVal)
+   
+   x <- data[,-c(ycol,scol)]
+   if (!allNumeric(x))
+      stop('X values must be numeric; use factorsToDummies on X')
+
+   if (!is.null(propensFtn)) {
+      if (propensFtn == 'lm') {
+         tmp <- lm(y ~ x)
+      } else {
+         tmp <- glm(y ~ x,family=binomial)
+      }
+      x <- tmp$fitted.values
+   }
+
+   matchOut <- Match(Y=y,Tr=s,X=x,estimand='ATE')
+
+   matchOut
+
+}
