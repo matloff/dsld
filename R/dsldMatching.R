0490fd7c955cfcf50d545db592be9cbf4f4d507f
Norm Matlof <nsmatloff@ucdavis.edu>
2024-06-08 15:57:48 -0700

    working on propensity

--- R/dsldMatching.R
+++ R/dsldMatching.R
@@ -27,22 +27,33 @@ dsldMatchedATE <- function(data,yName,sName,yesSVal,yesYVal=NULL,
    x <- data[,-c(ycol,scol)]
    if (!allNumeric(x))
       x <- factorsToDummies(x,omitLast=TRUE,dfOut=TRUE)
-
    if (!is.null(propensFtn)) {
       if (propensFtn == 'lm') {
          tmp <- lm(y ~ x)
       } else if (propensFtn == 'glm') {
          tmp <- glm(y ~ x,family=binomial)
       } else {  # qeML function
-         getSuggestedLib('qeML')
-         tmp <- do.call(propensFtn,
-            list(data=data[,-scol],yName=yName,holdout=NULL))$fitted.values
+         frmls <- names(formals(propensFtn))
+         if (!('yesYVal' %in% frmls)) {
+            tmp <- do.call(propensFtn,
+               list(data=data[,-scol],yName=yName,holdout=NULL))
+         } else {
+            tmp <- do.call(propensFtn,
+               list(data=data[,-scol],yName=yName,holdout=NULL,yesYVal=yesYVal))
+         }
+         x <- predict(tmp,data[,-c(ycol,scol)])
+         if (is.matrix(x)) x <- as.vector(x)
+         # yesSVal doesn't really matter here, just need values for
+         # matching
+         if (dichotY) {
+            if (propensFtn != 'qeKNN') {
+               x <- x$probs[,1]
+            } else x <- x$probs
+         }
       }
-      x <- tmp$fitted.values
    }
 
-   matchOut <- Matching::Match(Y=y,Tr=s,X=x,estimand='ATE')
-
+   matchOut <- Matching::Match(Y=y,Tr=s,X=x,estimand='ATE',ties=FALSE)
    matchOut
 
 }
