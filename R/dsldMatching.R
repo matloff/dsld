733e5be35745bc8429d59320d0531078bcdc5215
Norm Matlof <nsmatloff@ucdavis.edu>
2024-07-04 20:37:10 -0700

    works for numeric Y, now try dichot

--- R/dsldMatching.R
+++ R/dsldMatching.R
@@ -2,7 +2,7 @@
 # dsld wrapper for Matching::Match; optional propensFtn must be either
 # 'glm' for logit or 'knn' for qeKNN
 
-dsldMatchedATE <- function(data,yName,sName,yesSVal,yesYVal=NULL,
+dsldMatchedATE <- function(data,yName,sName,yesSVal,yesYVal=NULL,k=NULL,
    propensFtn=NULL) 
 {
    getSuggestedLib("Matching")
@@ -14,47 +14,36 @@ dsldMatchedATE <- function(data,yName,sName,yesSVal,yesYVal=NULL,
       yLvls <- levels(y)
       if (length(yLvls) != 2)
          stop('factor Y can only be dichotomous')
-      if (is.null(yesYVal))
-         stop('must specify yesYVal')
-      y <- as.integer(y == yesYVal)
+      yNum <- as.integer(y == yesYVal)
       dichotY <- TRUE
-   } else dichotY <- FALSE
+   } else {
+      yNum <- y
+      dichotY <- FALSE
+   }
+
 
    scol <- which(names(data) == sName)
    s <- data[,scol]
-   s <- as.integer(s == yesSVal)
+   sLog <- (s == yesSVal)
+   sNum <- as.integer(sLog)
    
    x <- data[,-c(ycol,scol)]
    if (!allNumeric(x))
-      x <- factorsToDummies(x,omitLast=TRUE,dfOut=TRUE)
-   if (!is.null(propensFtn)) {
-      ## if (propensFtn == 'lm') {
-      ##    tmp <- lm(y ~ x)
-      ## } else if (propensFtn == 'glm') {
-      if (propensFtn == 'glm') {
-         tmp <- glm(y ~ x,family=binomial)
-      } else {  # qeML function
-         frmls <- names(formals(propensFtn))
-         if (!('yesYVal' %in% frmls)) {
-            tmp <- do.call(propensFtn,
-               list(data=data[,-scol],yName=yName,holdout=NULL))
-         } else {
-            tmp <- do.call(propensFtn,
-               list(data=data[,-scol],yName=yName,holdout=NULL,yesYVal=yesYVal))
-         }
-         x <- predict(tmp,data[,-c(ycol,scol)])
-         if (is.matrix(x)) x <- as.vector(x)
-         # yesSVal doesn't really matter here, just need values for
-         # matching
-         if (dichotY) {
-            if (propensFtn != 'qeKNN') {
-               x <- x$probs[,1]
-            } else x <- x$probs
-         }
-      }
-   }
+      xNum <- factorsToDummies(x,omitLast=TRUE,dfOut=TRUE)
 
-   matchOut <- Matching::Match(Y=y,Tr=s,X=x,estimand='ATE',ties=FALSE)
+   if (!is.null(propensFtn)) {
+      if (propensFtn == 'glm') {
+         matchVals <- glm(sNum ~ xNum,family=binomial)$fitted.values
+      } else {  # qeKNN 
+         tmp <- qeKNN(data[,-ycol],sName,yesYVal=yesSVal,k=k)
+         matchVals <- tmp$regests
+      }
+      X <- matchVals
+   } else X <- xNum
+
+
+
+   matchOut <- Matching::Match(Y=y,Tr=sLog,X=xNum,estimand='ATE',ties=FALSE)
    matchOut
 
 }
