408fd11af5c87ca2fefaa22c8786a72f376b778c
Norm Matlof <nsmatloff@ucdavis.edu>
2024-07-26 21:22:41 -0700

    rewrote R code to use do.call, updated man pg

--- R/dsldML.R
+++ R/dsldML.R
@@ -1,25 +1,20 @@
 
-# like dsldLinear and dsldLogit, but for machine learning prediction algorithms
+# like dsldLinear and dsldLogit, but for machine learning (i.e.
+# nonparametric) prediction algorithms
 
 # args:
 
-#    dataName (must be quoted!), yName (must be called from quote()),
-#    sName as usual 
+#    data!, yName, sName as usual 
 
 #    sComparisonPts as in the with-interactions case of dsldLinear()
+#    (nonparametric case necessarily has interactions)
 
-#    qeMLftnName is, e.g. 'qeKNN'); opts is an R list of optional arguments
+#    qeMLftnName is, e.g. 'qeKNN'; opts is an R list of optional arguments
 #    for that function
 
-dsldML<-function(data,yName,sName,sComparisonPts='rand5',
-   qeMLftnName,opts=NULL,holdout=NULL){
+dsldML<-function(data,yName,sName,qeMLftnName,sComparisonPts='rand5',
+   opts=NULL,holdout=NULL){
 
-  # args checking
-  # if (!inherits(yName,'name')) stop('specify yName via quote()')
-  
-  
-  # execute params
-  data <- get(dataName)
   ycol <- which(names(data) == yName)
   scol <- which(names(data) == sName)
   slevels <- levels(data[,scol])
@@ -37,11 +32,9 @@ dsldML<-function(data,yName,sName,sComparisonPts='rand5',
   {
     subData <- data[data[,scol]==sLevel,]
     subData <- subData[,-scol]
-    qeMLftn <- get(qeMLftnName)
-    opts[['data']] <- data
+    opts[['data']] <- subData
     opts[['yName']] <- yName
-    opts[['sName']] <- sName
-    do.call(qeMLftn,opts)
+    do.call(qeMLftnName,opts)
   }
   
   qeOut <- lapply(slevels,do1Slevel)
