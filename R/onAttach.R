e2b23b46fad26abab866022465b4ddf45d318464
Norm Matlof <nsmatloff@ucdavis.edu>
2024-09-27 11:06:12 -0700

    quick start, ftn list

---
+++ R/onAttach.R
@@ -0,0 +1,9 @@
+
+.onAttach <- function(libname, pkgname) {
+   packageStartupMessage(
+      '\n\n\n\n\n*********************\n\n\n\n  Navigating dsld:\n
+      Type vignette("Quick_Start",package="dsld") for a quick overview!\n
+      Type vignette("Function_List",package="dsld") for a categorized function list\n
+      Latest version at https://github.com/matloff/dsld')
+}
+
